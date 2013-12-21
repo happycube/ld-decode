5058b5a804eeecef8bcfe9a42f2ef5a30496786c
happycube <Chad.Page@gmail.com>
2013-12-20 19:56:33 -0800

    cleaned up a little, added fast atan2

--- ld-decoder-10fsc.cxx
+++ ld-decoder-10fsc.cxx
@@ -24,28 +24,6 @@ double ctor(double r, double i)
 	return sqrt((r * r) + (i * i));
 }
 
-inline double dftc(double *buf, int offset, int len, double bin, double &fc, double &fci) 
-{
-	fc = 0.0; fci = 0.0;
-
-	for (int k = (-len + 1); k < len; k++) {
-//		cout << offset + k << ' ' << len << endl;
-		double o = buf[offset + k]; 
-		
-		fc += (o * cos((2.0 * M_PIl * ((double)(offset - k) / bin)))); 
-		fci -= (o * sin((2.0 * M_PIl * ((double)(offset - k) / bin)))); 
-	}
-
-	return ctor(fc, fci);
-}
-
-inline double dft(double *buf, int offset, int len, double bin) 
-{
-	double fc, fci;
-
-	return dftc(buf, offset, len, bin, fc, fci);
-}
-
 class Filter {
 	protected:
 		int order;
@@ -143,26 +121,44 @@ const double f_boost16_b[] {3.123765469711817e-03, 2.997477562454424e-03, 3.7500
 // fir1(15, (4.0/freq), 'hamming');
 const double f_lpf40_16_hamming_b[] {-2.028767853690441e-03, -5.146764387302929e-03, -9.901392487754552e-03, -8.028961431539007e-03, 1.455573714480611e-02, 6.572472577779680e-02, 1.357376803746136e-01, 1.977678364433565e-01, 2.226398128394282e-01, 1.977678364433565e-01, 1.357376803746136e-01, 6.572472577779684e-02, 1.455573714480611e-02, -8.028961431539007e-03, -9.901392487754554e-03, -5.146764387302935e-03, -2.028767853690441e-03};
 
-const double f_lpf40_32_hamming_b[] {-1.5652363638468312e-03, -1.6478881564047881e-03, -9.2742743690339626e-04, 1.2777474036302190e-03, 4.9093082118189680e-03, 8.1341641447971388e-03, 7.5421180415871049e-03, 1.3228248202934751e-04, -1.3715675929761275e-02, -2.8224201974851419e-02, -3.3068670365790559e-02, -1.7577386171408627e-02, 2.3359994514177564e-02, 8.4806879207920474e-02, 1.5161797160788967e-01, 2.0345139860864575e-01, 2.2298924435294154e-01, 2.0345139860864575e-01, 1.5161797160788965e-01, 8.4806879207920460e-02, 2.3359994514177564e-02, -1.7577386171408627e-02, -3.3068670365790580e-02, -2.8224201974851423e-02, -1.3715675929761277e-02, 1.3228248202934624e-04, 7.5421180415871049e-03, 8.1341641447971475e-03, 4.9093082118189697e-03, 1.2777474036302192e-03, -9.2742743690339712e-04, -1.6478881564047894e-03, -1.5652363638468312e-03};
-
 // freq = freq =5.0*(315.0/88.0)
 
-// [n, Wc] = buttord((4.5 / freq), (6 / freq), 3, 15); [b, a] = butter(n, Wc)
-const double f_lpf_6_a[] {1.0000000000000000e+00, -2.9603188604519133e+00, 4.0945616955978696e+00, -3.2164775768368816e+00, 1.4931150232709849e+00, -3.8399487942244304e-01, 4.2481926938480144e-02};
-const double f_lpf_6_b[] {1.0838645171265180e-03, 6.5031871027591073e-03, 1.6257967756897768e-02, 2.1677290342530360e-02, 1.6257967756897768e-02, 6.5031871027591073e-03, 1.0838645171265180e-03};
-
-// [n, Wc] = buttord((5 / freq), (7 / freq), 5 , 15); [b, a] = butter(n, Wc); freqz(b, a)
-
-const double f_lpf_4_a[] {1.000000000000000e+00, -1.916561071802124e+00, 1.669597410778569e+00, -6.887584214404641e-01, 1.135434590890410e-01};
-const double f_lpf_4_b[] {1.111383603906385e-02, 4.445534415625538e-02, 6.668301623438308e-02, 4.445534415625538e-02, 1.111383603906385e-02}; 
-
-//Filter f_lpf525(32, NULL, f_lpf525_32_hamming_b);	
-//Filter f_lpf40(32, NULL, f_lpf40_32_hamming_b);
 Filter f_lpf40(16, NULL, f_lpf40_16_hamming_b);
 
-Filter f_lpfb4(4, f_lpf_4_a, f_lpf_4_b);
-
 typedef vector<complex<double>> v_cossin;
+
+// From http://lists.apple.com/archives/perfoptimization-dev/2005/Jan/msg00051.html.  Used w/o permission, but will replace when
+// going integer... probably!
+const double PI_FLOAT = M_PIl;
+const double PIBY2_FLOAT = (M_PIl/2.0); 
+// |error| < 0.005
+double fast_atan2( double y, double x )
+{
+	if ( x == 0.0f )
+	{
+		if ( y > 0.0f ) return PIBY2_FLOAT;
+		if ( y == 0.0f ) return 0.0f;
+		return -PIBY2_FLOAT;
+	}
+	double atan;
+	double z = y/x;
+	if ( fabs( z ) < 1.0f )
+	{
+		atan = z/(1.0f + 0.28f*z*z);
+		if ( x < 0.0f )
+		{
+			if ( y < 0.0f ) return atan - PI_FLOAT;
+			return atan + PI_FLOAT;
+		}
+	}
+	else
+	{
+		atan = PIBY2_FLOAT - z/(z*z + 0.28f);
+		if ( y < 0.0f ) return atan - PI_FLOAT;
+	}
+	return atan;
+}
+
 
 class FM_demod {
 	protected:
@@ -213,7 +209,8 @@ class FM_demod {
 			for (int i = 0; i < 40; i++) avglevel[i] = 30;
 			for (int i = 0; i < 9; i++) cbuf[i] = 8100000;
 
-			min_offset = 128;
+			// 16-order pre, 16-order ...
+			min_offset = 40;
 		}
 
 		~FM_demod() {
@@ -255,17 +252,7 @@ class FM_demod {
 
 					double at2;
 
-#if 0
-					at2 = atan2(fci, fcq);	
-#else
-					if (!fcq) {
-						at2 = 0;
-					} else {
-						at2 = atan(fci/fcq);
-						if (fcq < 0) at2 = ((fci < 0) ? -M_PI : M_PI) + at2;			
-					}
-#endif
-//					cerr << fci << ' ' << fcq << ' ' << at2 << ' ' << atan(fci/fcq) << ' ' << at << endl;
+					at2 = fast_atan2(fci, fcq);	
 
 					level[j] = ctor(fci, fcq);
 	
@@ -290,15 +277,11 @@ class FM_demod {
 				if (f_post) thisout = f_post->feed(pf);	
 				if (i > min_offset) {
 					int bin = (thisout - 7600000) / 200000;
-					if (1 || bin < 0) bin = 0;
+					if (bin < 0) bin = 0;
 
 					avglevel[bin] *= 0.9;
 					avglevel[bin] += level[npeak] * .1;
 
-//					if (fabs(shift) > 50000) thisout += shift;
-//					cerr << ' ' << thisout << endl ;
-//					out.push_back(((level[npeak] / avglevel[bin]) * 1200000) + 7600000); 
-//					out.push_back((-(level[npeak] - 30) * (1500000.0 / 10.0)) + 9300000); 
 //					out.push_back(((level[npeak] / avglevel[bin]) > 0.3) ? thisout : 0);
 					out.push_back(thisout);
 				};
@@ -346,13 +329,7 @@ int main(int argc, char *argv[])
 	Filter f_afilt16(16, NULL, f_afilt16_b);
 	Filter f_boost16(16, NULL, f_boost16_b);
 
-//	vector<double> fb({7600000, 8100000, 8300000, 8500000, 8700000, 8900000, 9100000, 9300000}); 
-//	vector<double> fb({7600000, 8100000, 8600000, 9100000}); 
-	vector<double> fb({7600000, 8100000, 8700000, 9100000}); 
-	//vector<double> fb({8500000}); 
-
 	FM_demod video(4096, {7600000, 8100000, 8400000, 8700000, 9000000, 9300000}, &f_boost16, {&f_lpf40, &f_lpf40, &f_lpf40, &f_lpf40, &f_lpf40, &f_lpf40, &f_lpf40, &f_lpf40}, NULL);
-//	FM_demod video(2048, {7600000, 8100000, 8700000, 9300000}, &f_boost16, {&f_lpfb4, &f_lpfb4, &f_lpfb4, &f_lpfb4}, NULL);
 
 	double deemp[10], orig[10];
 
@@ -376,13 +353,9 @@ int main(int argc, char *argv[])
 				int entry = i % 9;
 				double diff = n - deemp[entry];
 
-//				cerr << n << ' ' << diff << ' ' << (n - 9300000) / diff << ' ' << (n - 8100000) / diff;
 				orig[entry] = n;
 	 			n -= (diff * (1.0/3.0));
-///	 			n -= (diff * .4);
-//	 			n -= (diff * (fabs(diff) / 4800000));
 				deemp[entry] = n;
-//				cerr << ' ' << n << endl;
 
 				n -= 7600000.0;
 				n /= (9300000.0 - 7600000.0);
@@ -390,7 +363,7 @@ int main(int argc, char *argv[])
 				in = 1 + (n * 62000.0);
 				if (in > 65535) in = 65535;
 			} else {
-				deemp[(i % 7)] = deemp[((i - 1) % 7)];
+				deemp[(i % 10)] = deemp[((i - 1) % 10)];
 				in = 0;
 			}
 
