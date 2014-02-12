60cddd078a5a764e44afe5ef0f3e5cdbd5d5c550
happycube <Chad.Page@gmail.com>
2014-02-11 22:25:42 -0800

    optimizations (bigger block size, more numpy functions) - still really slow, but only half as slow ;)

--- ld-decoder.py
+++ ld-decoder.py
@@ -5,7 +5,7 @@ import sys
 
 freq = (315.0 / 88.0) * 8.0
 freq_hz = freq * 1000000.0
-blocklen = 2048 
+blocklen = 32768 
 
 #bandpass_filter = sps.firwin(17, [0.10, 0.45])
 bandpass_filter = [3.023991564221081e-03, 4.233186409767337e-03, 7.954665760931824e-03, 2.061366484849445e-03, -1.422694634466230e-03, -7.408019315126677e-02, -1.359026202658482e-01, -6.450343643150648e-01, 1.689996991838728e+00, -6.450343643150648e-01, -1.359026202658483e-01, -7.408019315126678e-02, -1.422694634466230e-03, 2.061366484849445e-03, 7.954665760931824e-03, 4.233186409767340e-03, 3.023991564221081e-03]
@@ -17,25 +17,33 @@ lowpass_filter = sps.firwin(17, 3.5 / (freq / 2), window='hamming')
 #	print lowpass_filter[i], ",",  
 
 #print
-lowpass_filter = [-5.182956535966573e-04, -4.174028437151462e-03, -1.126381254549101e-02, -1.456598548706209e-02, 3.510439201231994e-03, 5.671595743858979e-02, 1.370914830220347e-01, 2.119161192395519e-01, 2.425762464437853e-01, 2.119161192395519e-01, 1.370914830220347e-01, 5.671595743858982e-02, 3.510439201231995e-03, -1.456598548706209e-02, -1.126381254549101e-02, -4.174028437151466e-03, -5.182956535966573e-04]
+#lowpass_filter = [-5.182956535966573e-04, -4.174028437151462e-03, -1.126381254549101e-02, -1.456598548706209e-02, 3.510439201231994e-03, 5.671595743858979e-02, 1.370914830220347e-01, 2.119161192395519e-01, 2.425762464437853e-01, 2.119161192395519e-01, 1.370914830220347e-01, 5.671595743858982e-02, 3.510439201231995e-03, -1.456598548706209e-02, -1.126381254549101e-02, -4.174028437151466e-03, -5.182956535966573e-04]
 
+bands = [8100000, 8700000, 9300000] 
+	
 def process(data):
 	# perform general bandpass filtering
 	in_len = len(data)
 	in_filt = sps.fftconvolve(data, bandpass_filter)
 
-	ohet = np.empty([len(bands), in_len], dtype=complex)
-	ohet_filt = np.empty([len(bands), in_len], dtype=complex)
-	ohet_filtn = np.empty([len(bands), in_len + 16], dtype=complex)
-	angles = np.empty([len(bands), in_len + 16])
-	dangle = np.empty([len(bands), in_len + 16])
-	levels = np.empty([len(bands), in_len + 16])
+	ohet = np.empty([len(bands), in_len + 16], dtype=complex)
+	ohet_filt = np.empty([len(bands), in_len + 16], dtype=complex)
+	ohet_filtn = np.empty([len(bands), in_len + 32], dtype=complex)
+	angles = np.empty([len(bands), in_len + 32])
+	dangle = np.empty([len(bands), in_len + 32])
+	abs_dangle = np.empty([len(bands), in_len + 32])
+	levels = np.empty([len(bands), in_len + 32])
 	output = np.empty(in_len)
+
+#	print len(in_filt) 
 
 	# heterodyning
 	for b in range(len(bands)):
-		for i in range(in_len): 
-			ohet[b][i] = in_filt[i] * het[b][i]	
+		if len(in_filt) == len(het[b]):
+			ohet[b] = np.multiply(in_filt, het[b])
+		else:
+			for i in range(in_len): 
+				ohet[b][i] = in_filt[i] * het[b][i]	
 
 	# lowpass filtering
 	for b in range(len(bands)):
@@ -53,6 +61,8 @@ def process(data):
 			
 			dangle[b][i] = adiff
 
+		abs_dangle[b] = np.fabs(dangle[b])
+
 	# select strongest signal
 	for i in range(0, in_len):
 		peaklevel = 500 
@@ -61,8 +71,8 @@ def process(data):
 #		print i, 
 		for b in range(len(bands)):
 #			print dangle[b][i], 
-			if (np.fabs(dangle[b][i]) < peaklevel):
-				peaklevel = np.fabs(dangle[b][i])
+			if (abs_dangle[b][i] < peaklevel):
+				peaklevel = abs_dangle[b][i]
 				peakband = b
 
 		output[i] = bands[peakband] - ((bands[peakband] / 2.0) * dangle[peakband][i])
@@ -72,15 +82,15 @@ def process(data):
 
 # setup
 
-bands = [8100000, 8700000, 9300000] 
+#bands = [8100000, 8700000, 9300000] 
 #bands = [8700000] 
 
 # need extra entries for heterodyning, aparently fftconvolve can lengthen the signal?  huh?
-het = np.empty([len(bands), blocklen + 128], dtype=complex)
+het = np.empty([len(bands), blocklen + 16], dtype=complex)
 
 for b in range(len(bands)):
 	fmult = bands[b] / freq_hz 
-	for i in range(blocklen + 128): 
+	for i in range(blocklen + 16): 
 		het[b][i] = complex(np.sin(i * 2.0 * np.pi * fmult), -(np.cos(i * 2.0 * np.pi * fmult))) 
 	print het[b][100],
 
@@ -93,7 +103,7 @@ infile = open("rd.raw", "rb")
 outfile = open("testpy.ld", "wb")
 #indata = []
 
-total = toread = 2048 
+total = toread = blocklen 
 inbuf = infile.read(toread)
 indata = np.fromstring(inbuf, 'uint8', toread)
 	
@@ -104,7 +114,7 @@ for i in range(0, 9):
 total = 0
 
 while len(inbuf) > 0:
-	toread = 2048 - indata.size 
+	toread = blocklen - indata.size 
 
 	if toread > 0:
 		inbuf = infile.read(toread)
