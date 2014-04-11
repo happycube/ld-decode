f511f1da60e13f6e73486fd31c97a750f814fbf9
happycube <Chad.Page@gmail.com>
2014-04-10 20:35:59 -0700

    now more (but not 100%) stable in pause frames

--- ntsc.cxx
+++ ntsc.cxx
@@ -13,6 +13,10 @@ const double f28_1_3mhz_b32[] {-1.605533065998730e-03, -1.720671809315438e-03, -
 
 const double f28_0_6mhz_b64[] {-6.916447903947148e-04, -6.637277886690091e-04, -6.506794962762819e-04, -6.385960636428408e-04, -6.091489627652988e-04, -5.401328736698201e-04, -4.062390816451122e-04, -1.800289567056259e-04, 1.669277273337949e-04, 6.627933750400666e-04, 1.334132570703104e-03, 2.204566737142542e-03, 3.293471104686198e-03, 4.614771600461567e-03, 6.175896724145871e-03, 7.976934496300239e-03, 1.001003732312394e-02, 1.225910839260336e-02, 1.469979236820074e-02, 1.729978111972153e-02, 2.001943252605971e-02, 2.281268753589040e-02, 2.562825822709219e-02, 2.841104809911676e-02, 3.110375576479802e-02, 3.364860502185666e-02, 3.598913834498529e-02, 3.807200741849585e-02, 3.984869359245655e-02, 4.127709314339044e-02, 4.232290688845818e-02, 4.296078085959773e-02, 4.317515410421566e-02, 4.296078085959773e-02, 4.232290688845819e-02, 4.127709314339045e-02, 3.984869359245655e-02, 3.807200741849585e-02, 3.598913834498529e-02, 3.364860502185667e-02, 3.110375576479803e-02, 2.841104809911677e-02, 2.562825822709219e-02, 2.281268753589041e-02, 2.001943252605972e-02, 1.729978111972153e-02, 1.469979236820075e-02, 1.225910839260336e-02, 1.001003732312394e-02, 7.976934496300244e-03, 6.175896724145871e-03, 4.614771600461570e-03, 3.293471104686198e-03, 2.204566737142541e-03, 1.334132570703105e-03, 6.627933750400653e-04, 1.669277273337959e-04, -1.800289567056260e-04, -4.062390816451116e-04, -5.401328736698201e-04, -6.091489627652993e-04, -6.385960636428407e-04, -6.506794962762823e-04, -6.637277886690096e-04, -6.916447903947148e-04};
 
+const double f28_0_3mhz_b32[] {3.978057329252118e-03, 4.515056281806121e-03, 5.964949733492637e-03, 8.323677232466895e-03, 1.154314080495843e-02, 1.553225223505762e-02, 2.016018845137591e-02, 2.526172777477888e-02, 3.064442643002365e-02, 3.609729304005547e-02, 4.140053457612618e-02, 4.633588526888740e-02, 5.069699391924866e-02, 5.429933707757621e-02, 5.698914631738589e-02, 5.865088633990866e-02, 5.921289437519849e-02, 5.865088633990864e-02, 5.698914631738591e-02, 5.429933707757621e-02, 5.069699391924866e-02, 4.633588526888740e-02, 4.140053457612618e-02, 3.609729304005548e-02, 3.064442643002365e-02, 2.526172777477887e-02, 2.016018845137590e-02, 1.553225223505763e-02, 1.154314080495844e-02, 8.323677232466895e-03, 5.964949733492642e-03, 4.515056281806124e-03, 3.978057329252118e-03};
+
+const double f28_0_3mhz_b64[] {1.156216942166937e-03, 1.260302595139044e-03, 1.439372164292797e-03, 1.703612390217742e-03, 2.062019858422272e-03, 2.522165397605361e-03, 3.089981612510836e-03, 3.769578440427629e-03, 4.563091102952233e-03, 5.470564206893792e-03, 6.489875042604241e-03, 7.616698349024995e-03, 8.844513978562953e-03, 1.016465801913355e-02, 1.156641703393353e-02, 1.303716418092476e-02, 1.456253509299260e-02, 1.612664055540294e-02, 1.771231222795924e-02, 1.930137694250046e-02, 2.087495447795009e-02, 2.241377318905297e-02, 2.389849745309321e-02, 2.531006061071641e-02, 2.662999691928111e-02, 2.784076601392927e-02, 2.892606348388255e-02, 2.987111141749148e-02, 3.066292314453501e-02, 3.129053690062719e-02, 3.174521374612856e-02, 3.202059577804195e-02, 3.211282146320453e-02, 3.202059577804194e-02, 3.174521374612856e-02, 3.129053690062719e-02, 3.066292314453502e-02, 2.987111141749148e-02, 2.892606348388254e-02, 2.784076601392927e-02, 2.662999691928112e-02, 2.531006061071642e-02, 2.389849745309320e-02, 2.241377318905297e-02, 2.087495447795010e-02, 1.930137694250046e-02, 1.771231222795925e-02, 1.612664055540295e-02, 1.456253509299260e-02, 1.303716418092477e-02, 1.156641703393353e-02, 1.016465801913356e-02, 8.844513978562949e-03, 7.616698349024998e-03, 6.489875042604247e-03, 5.470564206893790e-03, 4.563091102952234e-03, 3.769578440427634e-03, 3.089981612510838e-03, 2.522165397605363e-03, 2.062019858422274e-03, 1.703612390217742e-03, 1.439372164292798e-03, 1.260302595139044e-03, 1.156216942166937e-03};
+
 const double f_hsync8[] {1.447786467971050e-02, 4.395811440315845e-02, 1.202636955256379e-01, 2.024216184054497e-01, 2.377574139720867e-01, 2.024216184054497e-01, 1.202636955256379e-01, 4.395811440315847e-02, 1.447786467971050e-02};
 
 inline double IRE(double in) 
@@ -218,7 +222,9 @@ class TBC
 	
 		int bufsize; 
 
-		double prev_adjust;
+		bool jumped;
+		double prev_gap, prev_adjust;
+
 		double curscale;
 
 		uint16_t frame[1820 * 530];
@@ -230,14 +236,14 @@ class TBC
 		int32_t framecode;	// in hex
 
 		int FindHSync(uint16_t *buf, int start, int len, int &pulselen, int tlen = 60) {
-			Filter f_s(32, NULL, f28_1_3mhz_b32);
+			Filter f_s(32, NULL, f28_0_3mhz_b32);
 
 			int sync_start = -1;
 
 			framecode = -1;
 
 			// allow for filter startup
-			if (start > 31) start -= 31;
+			if (start > 32) start -= 32;
 	
 			for (int i = start; i < start + len; i++) {
 				double v = f_s.feed(buf[i]);
@@ -245,7 +251,7 @@ class TBC
 //				cerr << i << ' ' << buf[i] << ' ' << v << endl; 
 
 				// need to wait 30 samples
-				if (i > 30) {
+				if (i > 32) {
 					if (sync_start < 0) {
 						if (v < 11000) sync_start = i;
 					} else if (v > 11000) {
@@ -260,6 +266,60 @@ class TBC
 			}
 			return -1;
 		} 
+		
+		int eBurstDetect(uint16_t *buf, int start, int len, double &plevel, double &pphase)
+		{
+			int rv = 0;
+			double pi = 0, pq = 0;
+
+			double i[4096], q[4096], level[4096];
+
+			plevel = 0.0;
+			pphase = 0.0;
+
+			f_synci->clear(ire_to_u16(black_ire));
+			f_syncq->clear(ire_to_u16(black_ire));
+
+			// XXX? allow for filter startup
+			if (start > 65) start -= 65;
+
+			for (int l = start; l < start + len; l++) {
+				double v = buf[l];
+
+//				v = (double)(v - black_u16) / (double)(white_u16 - black_u16); 
+
+				q[l] = f_syncq->feed(v * _cos[l % 8]);
+				i[l] = f_synci->feed(-v * _sin[l % 8]);
+
+				level[l] = ctor(i[l], q[l]);
+
+//				if ((l - start) > 65) cerr << l << ' ' << buf[l] << ' ' << level[l] << ' ' << atan2(i[l], q[l]) << ' ' << atan2(pi, pq) << endl;
+	
+				if (((l - start) > 65) && level[l] > plevel) {
+					plevel = level[l];
+					pi = i[l]; pq = q[l];
+				}
+			}
+	
+			double aphase = 0.0, threshold = plevel * .98;	
+			int valid = 0;	
+			for (int l = start + 65; l < start + len; l++) {
+				if (level[l] > threshold) {
+					aphase += atan2(i[l], q[l]);
+					valid++;
+//					cerr << aphase << ' ' << aphase / valid << endl;
+				}
+			}
+
+			if (plevel) {
+				pphase = aphase / valid;
+//				pphase = atan2(pi, pq);
+			}
+
+//			cerr << pi << ' ' << pq << ' ' << pphase << endl;
+
+			return rv;
+		}
 
 		int BurstDetect(uint16_t *buf, int start, int len, double &plevel, double &pphase)
 		{
@@ -283,7 +343,7 @@ class TBC
 
 				double level = ctor(i, q);
 
-//				if ((l - start) > 65) cerr << l << ' ' << buf[l] << ' ' << level << ' ' << atan2(i, q) << endl;
+		//		if ((l - start) > 65) cerr << l << ' ' << buf[l] << ' ' << level << ' ' << atan2(i, q) << endl;
 	
 				if (((l - start) > 65) && level > plevel) {
 					plevel = level;
@@ -387,10 +447,11 @@ class TBC
 			f_i = new Filter(32, NULL, f28_1_3mhz_b32);
 			f_q = new Filter(32, NULL, f28_1_3mhz_b32);
 
-                        f_synci = new Filter(64, NULL, f28_0_6mhz_b64);
-                        f_syncq = new Filter(64, NULL, f28_0_6mhz_b64);
+                        f_synci = new Filter(64, NULL, f28_0_3mhz_b64);
+                        f_syncq = new Filter(64, NULL, f28_0_3mhz_b64);
 
-			prev_adjust = 0;
+			jumped = false;
+			prev_gap = prev_adjust = 0;
 		}
 
 		// This assumes an entire 4K sliding buffer is available.  The return value is the # of new bytes desired 
@@ -403,8 +464,6 @@ class TBC
 			int sync_len;
 			int sync_start = FindHSync(buffer, 0, bufsize, sync_len);
 		
-			bool jumped = false;
-	
 			// if there isn't a whole line and (if applicable) following burst, advance first
 			if (sync_start < 0) {
 				scount += 4096;
@@ -447,11 +506,11 @@ class TBC
 
 				// determine color burst and phase levels of both this and next color bursts
 
-				BurstDetect(&buffer[sync_start], 3.5 * dots_usec, 7.5 * dots_usec, plevel, pphase);
+				BurstDetect(&buffer[sync_start], 4.5 * dots_usec, 7.0 * dots_usec, plevel, pphase);
 //				cerr << curline << " start " << sync_start << " burst 1 " << plevel << " " << pphase << endl;
 		
 				// cerr << sync_len << ' ' << (sync2_start - sync_start + sync2_len) << endl;	
-				BurstDetect(&buffer[sync_start], (sync2_start - sync_start) + (3.5 * dots_usec), 7.5 * dots_usec, plevel2, pphase2);
+				BurstDetect(&buffer[sync_start], (sync2_start - sync_start) + (4.5 * dots_usec), 7.0 * dots_usec, plevel2, pphase2);
 				// cerr << "burst 2 " << plevel2 << " " << pphase2 << ' ';
 
 				// if available, use the phase data of the next line's burst to determine line length
@@ -467,7 +526,7 @@ class TBC
 //					cerr << "gap " << gap << endl;
 					ScaleOut(buffer, outbuf, sync_start, 1820 + gap);
 				
-					BurstDetect(outbuf, 3.5 * dots_usec, 7.5 * dots_usec, plevel, pphase);
+					BurstDetect(outbuf, 4.5 * dots_usec, 7.0 * dots_usec, plevel, pphase);
 //					cerr << "gap " << gap << ' ' << "post-scale 1 " << plevel << " " << pphase << endl;
 				
 					// if this is the first line (of the frame?) set up phase targets for each line
@@ -480,7 +539,7 @@ class TBC
 					}
 				} else {
 					cerr << "WARN:  Missing burst\n";
-					gap = sync2_start - sync_start;
+					gap = 0.0; // sync2_start - sync_start;
 				}
 
 				// if this line has a good color burst, adjust phase
@@ -502,23 +561,35 @@ class TBC
 					double dadjust = fabs(adjust - prev_adjust);
 
 					// if we jumped a half phase, we might be repeating a frame
-					if ((dadjust > 3.5) && (dadjust < 4.5)) {
-//						cerr << "J" << linecount << ' ' << prev_adjust << ' ' << adjust << endl;
+					if (((dadjust > 3.5) && (dadjust < 6.0)) || 
+					    (!jumped && (fabs(gap - prev_gap) > 2.0))) { 
+	
+						cerr << "J" << linecount << ' ' << prev_adjust << ' ' << adjust << ' ' << gap << ' ' << prev_gap << endl;
 						jumped = true;
 						linecount++;
+					} else {
+						jumped = false;
 					}
-
-//					cerr << "adjust " << adjust << " gap " << gap << endl;
+			
+	
+					if (dadjust > 7.0) {
+//						cerr << "E";
+						if (prev_adjust < 0) adjust -= 8;
+						else adjust += 8;
+					}
+/*
 					if (adjust < -4) {
+						cerr << "e";
 						adjust += 8;
 					}
 					if (adjust > 4) {
+						cerr << "e";
 						adjust -= 8;
 					}
-//					cerr << "adjust " << adjust << " gap " << gap << endl;
+*/
+//					cerr << linecount << " adjust " << adjust << " gap " << gap << " syncgap " << (sync2_start - sync_start) << endl;
 
                                         ScaleOut(buffer, outbuf, sync_start + adjust, 1820 + gap);
-                                        BurstDetect(outbuf, 3.5 * dots_usec, 7.5 * dots_usec, plevel, pphase);
 				
 					prev_adjust = jumped ? 0 : adjust;
 				} else {
@@ -582,6 +653,8 @@ class TBC
 				}
 			}
 			if (linecount >= 0) linecount++;
+
+			prev_gap = gap;
 
 			scount += sync_start - 64 + 1820;
 			return (sync_start - 64 + 1820);
