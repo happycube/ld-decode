63c13a42185b2218056c2b9749e56b99b716e346
happycube <Chad.Page@gmail.com>
2014-08-23 15:45:13 -0700

    fixed a bit (but can still be fooled, i bet!) - slight SNR increase over C version, but slower

--- ntsc.py
+++ ntsc.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 import scipy as sp
 import scipy.signal as sps
@@ -11,7 +12,7 @@ freq = 8.0
 
 freq_mhz = (315.0 / 88.0) * freq
 freq_hz = freq * 1000000.0
-blocklen = 5599230 / 2
+blocklen = 65536 * 8 
 
 # inframe is a 1685x505 uint16 buffer.  basically an ntsc frame with syncs removed
 
@@ -55,6 +56,9 @@ for i in range(0, 8):
 
 burst_len = 300
 
+def printerr(*objs):
+    print(*objs, file=sys.stderr)
+
 def burst_detect(line):
 	level = 0
 	phase = 0
@@ -62,7 +66,8 @@ def burst_detect(line):
 	obhet = np.empty(burst_len, dtype=np.complex)
 	obhet = np.empty(1820, dtype=np.complex)
 	#for i in range(0, burst_len):
-	for i in range(8, 300):
+
+	for i in range(8, 240):
 		obhet[i] = bhet[i % 8] * line[i]
 
 #	obhet_filt = sps.fftconvolve(obhet, color_filter)
@@ -144,7 +149,6 @@ def comb(inframe):
 	row = np.empty([1685 + 32], dtype=np.complex)
 
 	for l in range(24, 504):
-		print l
 
 		# compute 2D and adjustment arrays
 		rowa = (0.5 * lohet_filt[l]) + (0.25 * lohet_filt[l - 2]) + (0.25 * lohet_filt[l + 2]) 
@@ -162,8 +166,8 @@ def comb(inframe):
 		agreen = np.fabs(diffn - 1)
 
 		for i in range(155, 155 + 1488):
-			if (l == 60) or (l == 61):
-				print i, lohet_filt[l][i], a[i], agreep[i], agreen[i] 
+#			if (l == 60) or (l == 61):
+#				print i, lohet_filt[l][i], a[i], agreep[i], agreen[i] 
 
 			if (a[i] < 400):
 				row[i] = lohet_filt[l][i] 
@@ -238,6 +242,9 @@ level_120ire = 65535;
 from scipy import interpolate
 
 def getline(l):
+	if (l != np.floor(l)):
+		l = l + 0.5
+
 	if (l < 10):
 		return -1
 
@@ -247,9 +254,24 @@ def getline(l):
 	if (l < 271):
 		return -1
 	
+	if (l > 524):
+		return -1
+	
 	return ((l - 273) * 2) + 1 
 
-def scale(buf, begin, end):
+# essential constants
+freq = 8.0
+ntsc_linelength = 227.5 * freq
+# this is the hblank interval except for front porch
+ntsc_blanklen = 9.2
+ntsc_hsynctoline = np.floor(ntsc_linelength * ntsc_blanklen / 63.5) 
+
+scale_linelen = ((63.5 + ntsc_blanklen) / 63.5)	# add to non-TBC line length to get second hblank
+scale_tgt = ntsc_linelength + ntsc_hsynctoline
+
+#frame = np.empty([505, 1685 + ntsc_hsynctoline], dtype=np.uint16)
+
+def scale(buf, begin, end, tgtlen):
 	ibegin = np.floor(begin)
 	iend = np.floor(end)
 
@@ -259,14 +281,26 @@ def scale(buf, begin, end):
 	arr = np.linspace(0, dist, num=dist)
 #	print len(arr), dist #, len(buf[np.floor(crosspoint):np.floor(crosspoint) + dist])
 	spl = interpolate.splrep(arr, buf[ibegin:ibegin + dist])
-	arrout = np.linspace(begin - ibegin, linelen, 1820)
+	arrout = np.linspace(begin - ibegin, linelen, tgtlen)
 						
 	return np.clip(interpolate.splev(arrout, spl), 0, 65535)
+
+def wrap_angle(angle, tgt):
+	adjust = tgt - angle
+	if (adjust > (np.pi)):
+		adjust -= 2 * np.pi
+	elif (adjust < (-np.pi)):
+		adjust += 2 * np.pi
+
+	return adjust
+
+line = -2
+tgt_phase = 0
 
 def find_sync(buf):
 	count = 0
 	numsyncs = 0
-	line = -2
+	global line, tgt_phase
 
 #	print buf
 	filtered = sps.fftconvolve(buf, sync_filter)[len(sync_filter) / 2:]
@@ -274,7 +308,7 @@ def find_sync(buf):
 	cross = 5000
 	prev_crosspoint = -1
 
-	for i in range(0, len(filtered)):
+	for i in range(0, len(filtered) - 4096):
 #		print i, filtered[i], buf[i], buf[i + (len(sync_filter) / 2)]
 		if (filtered[i] < cross):
 			if (count == 0):
@@ -284,7 +318,6 @@ def find_sync(buf):
 				crosspoint = i + c
 
 				#print c, d, filtered[i - 1], filtered[i]
-				
 				
 			count = count + 1
 		else:
@@ -296,34 +329,68 @@ def find_sync(buf):
 
 				l = buf[i:i + 1820]
 	#			print burst_detect(l)
-				
+
 				if (prev_crosspoint > 0):
-					linelen = crosspoint - prev_crosspoint
+					begin = prev_crosspoint
+					end = crosspoint
+					linelen = end - begin
+
+					if (crosspoint + (linelen * scale_linelen) > len(buf)):
+						return np.floor(prev_crosspoint) - 100 
+
 #					print crosspoint, prev_crosspoint, crosspoint - prev_crosspoint, count
 					if (line >= 0) and (linelen > 1800) and (count > 90):
 						outl = getline(line)
 
-						out = scale(buf, prev_crosspoint, crosspoint)
+#						print line, outl
+						print(line, outl, file=sys.stderr) 
+
+						out = scale(buf, begin, begin + (linelen * scale_linelen), scale_tgt)
 
 						angle = burst_detect(out)[1]
-						if (angle < 0):
-							adjust = (-np.pi / 2) - angle
+						angle2 = burst_detect(out[linelen:len(out)])[1]
+
+						if tgt_phase:
+							tgt_phase = -tgt_phase
+						elif angle < 0 and (angle > -(3 * np.pi / 4)):
+							tgt_phase = -np.pi / 2
 						else:
-							adjust = (np.pi / 2) - angle
+							tgt_phase = np.pi / 2
+	
+						adjust = wrap_angle(angle, tgt_phase) 
+						
+#						print outl, angle, angle2, adjust
+						printerr(outl, angle, angle2, adjust)
+
+						begin = begin + (adjust * 1.2)
+						end = end + (adjust * 1.2)
+						linelen = end - begin
+						
+						#out = scale(buf, prev_crosspoint, crosspoint, linelen)
+#						printe(prev_crosspoint, prev_crosspoint + (linelen * scale_linelen), scale_tgt
+						out = scale(buf, begin, begin + (linelen * scale_linelen), scale_tgt)
+						
+						angle = burst_detect(out)[1]
+						angle2 = burst_detect(out[linelen:len(out)])[1]
+						
+						adjust2 = wrap_angle(angle2, -tgt_phase) 
+						printerr(outl, angle, angle2, adjust2)
+						
+#						end = end - (adjust2 * 1.0)
+						linelen = end - begin
+						
+						out = scale(buf, begin, begin + (linelen * scale_linelen), scale_tgt)
 						
 						output_16 = np.empty(len(out), dtype=np.uint16)
 						np.copyto(output_16, out, 'unsafe')
-
-						prev_crosspoint = prev_crosspoint + (adjust * 1.0)
-#						crosspoint = crosspoint + (adjust * 1.3)
 						
-						out = scale(buf, prev_crosspoint, crosspoint)
-						
-					#	output_16 = np.empty(len(out), dtype=np.uint16)
-						np.copyto(output_16, out, 'unsafe')
+						angle = burst_detect(out)[1]
+						angle2 = burst_detect(out[linelen:len(out)])[1]
+#						print outl, angle, angle2
 
 						outl = getline(line)
 						if (outl >= 0):	
+							#frame[outl] = output_16[130:130+1685+ntsc_hsynctoline]
 							frame[outl] = output_16[130:130+1685]
 #						print line, begin, outl, burst_detect(out), angle, burst_detect(out)[1], adjust
 	
@@ -331,22 +398,30 @@ def find_sync(buf):
 					
 						line = line + 1
 					else: 
-						if ((line == -1) or (line > 520)) and (linelen > 1800) and (count < 80):
+						if ((line == -1) and (linelen < 1000) and (count > 80) and (count < 160)):
+							line = 266.5
+						elif ((line == -1) or (line > 520)) and (linelen > 1800) and (count < 80):
 							if (line > 0):
 								sys.stdout.write(frame)
 							line = 1
+							tgt_phase = 0
 						elif (line == -2) and (linelen > 1800) and (count > 80):
 							line = -1
 						elif line >= 0 and (linelen > 800) and (linelen < 1000):
 							line = line + 0.5
 						elif line >= 0 and (linelen > 1700):
 							line = line + 1
+						print(line, prev_crosspoint, crosspoint, count, file=sys.stderr) 
+
+						# hack
+						if (np.floor(line) == 272):
+							tgt_phase = -tgt_phase
 	
 				prev_crosspoint = crosspoint
 
 			count = 0
 
-	return np.floor(prev_crosspoint) 
+	return np.floor(prev_crosspoint) - 100 
 	
 outfile = open("test.rgb", "wb")
 infile = sys.stdin
@@ -359,7 +434,7 @@ indata = np.fromstring(inbuf, 'uint16', toread / 2)
 #print toread
 
 while len(inbuf) > 0:
-	toread = (blocklen - len(indata)) 
+	toread = (blocklen - len(indata)) * 2 
 	while toread > 0:
 		inbuf = infile.read(toread)
 		indata = np.append(indata, np.fromstring(inbuf, 'uint16', toread / 2))
@@ -369,13 +444,13 @@ while len(inbuf) > 0:
 #		print len(inbuf), toread
 
 #	print toread
-	find_sync(indata)
+	keep = find_sync(indata) * 1
 
 #	print len(indata)
 #	inframe = np.reshape(indata, (505, 1685))	
 #	rgbout = process(inframe)
 
 #	indata = np.delete(indata, np.s_[0:len(output)])
-	indata = np.array([])
+	indata = indata[keep:len(indata)]
 
-	exit()
+#	exit()
