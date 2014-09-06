b705d723f5675414b6ccc01d0a81f895dc6bb95f
happycube <Chad.Page@gmail.com>
2014-09-05 22:15:55 -0700

    change x res to 852 to have some extra bits

--- ntscg2.cxx
+++ ntscg2.cxx
@@ -20,21 +20,30 @@ void aclamp(double *v, int len, double low, double high)
 }
 
 // NTSC properties
-const double freq = 8.0;	// in FSC.  Must be an even number!
+const double in_freq = 8.0;	// in FSC.  Must be an even number!
+#define OUT_FREQ 4
+const double out_freq = OUT_FREQ;	// in FSC.  Must be an even number!
 
 const double ntsc_uline = 63.5; // usec_
-const double ntsc_pline = 227.5 * freq; // pixels per line
-const int    ntsc_plinei = (int)ntsc_pline;
-const double dotclk = (1000000.0*(315.0/88.0)*freq); 
+const double ntsc_ipline = 227.5 * in_freq; // pixels per line
+const double ntsc_opline = 227.5 * out_freq; // pixels per line
+const int ntsc_oplinei = 227.5 * out_freq; // pixels per line
+const double dotclk = (1000000.0*(315.0/88.0)*in_freq); 
 
 const double dots_usec = dotclk / 1000000.0; 
 
 const double ntsc_blanklen = 9.2;
-const double ntsc_hsynctoline = ntsc_pline * (ntsc_blanklen / 63.5);
 
 // we want the *next* colorburst as well for computation 
 const double scale_linelen = ((63.5 + ntsc_blanklen) / 63.5); 
-const double scale_tgt = ntsc_pline + ntsc_hsynctoline;
+
+const double ntsc_ihsynctoline = ntsc_ipline * (ntsc_blanklen / 63.5);
+const double iscale_tgt = ntsc_ipline + ntsc_ihsynctoline;
+
+const double ntsc_hsynctoline = ntsc_opline * (ntsc_blanklen / 63.5);
+const double scale_tgt = ntsc_opline + ntsc_hsynctoline;
+
+const double phasemult = 1.591549430918953e-01 * in_freq;
 
 // uint16_t levels
 uint16_t level_m40ire = 1;
@@ -100,39 +109,53 @@ double black_ire = 7.5;
 
 int write_locs = -1;
 
-uint16_t frame[505][1685];
+uint16_t frame[505][(int)(OUT_FREQ * 213)];
 
 // set up sync color heterodyne table first 
 complex<double> burst_hdyne[2400];
 
-double _cos[16], _sin[16];
-
 Filter f_syncr(f_sync);
 Filter f_synci(f_sync);
 
-Filter f_bpcolor(f_colorbp);
+Filter f_bpcolor4(f_colorbp4);
+Filter f_bpcolor8(f_colorbp8);
 		
-void BurstDetect(double *line, int loc, double &plevel, double &pphase) 
+void BurstDetect(double *line, int freq, double _loc, double &plevel, double &pphase) 
 {
+	double _cos[freq], _sin[freq];
 	double pi = 0, pq = 0, ploc = -1;
-	int len = 200;
+	int len = (25 * freq);
+	int loc = _loc * freq;
+
+	Filter *f_bpcolor;
 
 	plevel = 0.0;
 	pphase = 0.0;
 
 	f_syncr.clear(0);
 	f_synci.clear(0);
-	f_bpcolor.clear(0);
+	
+	for (int e = 0; e < freq; e++) {
+		_cos[e] = cos((2.0 * M_PIl * ((double)e / freq)));
+		_sin[e] = sin((2.0 * M_PIl * ((double)e / freq)));
+	}
 
-	for (int l = loc + 110; l < loc + len; l++) {
-		double v = f_bpcolor.feed(line[l]);
+	if (freq == 4) {
+		f_bpcolor = &f_bpcolor4;
+	} else {
+		f_bpcolor = &f_bpcolor8;
+	}
+	f_bpcolor->clear(0);
 
-		double q = f_syncr.feed(v * _cos[l % 8]);
-		double i = f_synci.feed(-v * _sin[l % 8]);
+	for (int l = loc + (14 * freq); l < loc + len; l++) {
+		double v = f_bpcolor->feed(line[l]);
+
+		double q = f_syncr.feed(v * _cos[l % freq]);
+		double i = f_synci.feed(-v * _sin[l % freq]);
 
 		double level = ctor(i, q);
 
-//		cerr << l << ' ' << line[l] << ' ' << v << ' ' << i << ' ' << q << ' ' << level << endl;
+	//	cerr << l << ' ' << line[l] << ' ' << v << ' ' << i << ' ' << q << ' ' << level << ' ' << atan2(i, q) << endl;
 
 		if (((l - loc) > 16) && (level > plevel) && (level < 10000)) {
 			ploc = l;
@@ -188,17 +211,16 @@ int Process(uint16_t *buf, int len)
 			count++;
 		} else {
 			if (count > 40) crosspoint = tmp_crosspoint;
+
 			if ((count > 30) && prev_crosspoint > 0) {
 				double begin = prev_crosspoint;
 				double end = begin + ((crosspoint - prev_crosspoint) * scale_linelen);
 				double linelen = crosspoint - prev_crosspoint; 
 
-				double tout1[4096];
-				double tout2[4096];
-				double tout3[4096];
+				double tout1[4096], tout2[4096], tout3[4096];
 
 				cerr << line << ' ' << i << ' ' << linelen << ' ' << count << endl;
-				if ((line >= 0) && (linelen >= (ntsc_pline * 0.9)) && (count > 90)) {
+				if ((line >= 0) && (linelen >= (ntsc_ipline * 0.9)) && (count > (11 * in_freq))) {
 					// standard line
 					double plevel1, pphase1;
 					double plevel2, pphase2;
@@ -208,58 +230,76 @@ int Process(uint16_t *buf, int len)
 					if (firstline < 0) firstline = line;
 
 					Scale(buf, tout1, begin, end, scale_tgt); 
-					BurstDetect(tout1, 0, plevel1, pphase1); 
-					BurstDetect(tout1, 1822, plevel2, pphase2); 
+					BurstDetect(tout1, out_freq, 0, plevel1, pphase1); 
+					BurstDetect(tout1, out_freq, 228, plevel2, pphase2); 
 
 					if (!tgt_phase) {
 						if ((pphase1 < 0) && (pphase1 > (-M_PIl * 3 / 4))) {
-							tgt_phase = -M_PIl / 2.0;
+							tgt_phase = (-M_PIl / 2.0);
 						} else {
-							tgt_phase = M_PIl / 2.0;
+							tgt_phase = (M_PIl / 2.0);
 						}
 					} else tgt_phase = -tgt_phase;
-				
+			
 					if (floor(line) == 272) tgt_phase = -tgt_phase;
 
+					double atgt_phase = tgt_phase - (57 * (M_PIl / 180));
+
+					if (tgt_phase > 0) 
+						atgt_phase = (-180 + 0) * (M_PIl / 180);
+					else
+						atgt_phase = (0 + 0) * (M_PIl / 180);
+
+			//		atgt_phase = tgt_phase + (33 * (M_PIl / 180));
+
 					cerr << line << " 0" << ' ' << ((end - begin) / scale_tgt) * 1820.0 << ' ' << plevel1 << ' ' << pphase1 << ' ' << pphase2 << endl;
-					adjust1 = WrapAngle(pphase1, tgt_phase);	
-					adjust2 = WrapAngle(pphase2, tgt_phase);
-					begin += (adjust1 * 1.2732);
-					end += (adjust1 * 1.2732);
+					adjust1 = WrapAngle(pphase1, atgt_phase);	
+					adjust2 = WrapAngle(pphase2, atgt_phase);
+					begin += (adjust1 * phasemult);
+					end += (adjust1 * phasemult);
 
 					Scale(buf, tout2, begin, end, scale_tgt); 
-					BurstDetect(tout2, 0, plevel1, pphase1); 
-					BurstDetect(tout2, 1822, plevel2, pphase2); 
+					BurstDetect(tout2, out_freq, 0, plevel1, pphase1); 
+					BurstDetect(tout2, out_freq, 228, plevel2, pphase2); 
 					
 					cerr << line << " 1" << ' ' << ((end - begin) / scale_tgt) * 1820.0 << ' ' << plevel1 << ' ' << pphase1 << ' ' << pphase2 << endl;
 
-					adjust1 = WrapAngle(pphase1, tgt_phase);	
+					adjust1 = WrapAngle(pphase1, atgt_phase);	
 					adjust2 = WrapAngle(pphase2, pphase1);
-					end += (adjust2 * 1.2732);
+					end += (adjust2 * phasemult);
 					
 					Scale(buf, tout3, begin, end, scale_tgt); 
-					
-					BurstDetect(tout3, 0, plevel1, pphase1); 
-					BurstDetect(tout3, 1822, plevel2, pphase2); 
+					BurstDetect(tout3, out_freq, 0, plevel1, pphase1); 
+					BurstDetect(tout3, out_freq, 228, plevel2, pphase2); 
 
 					cerr << line << " 2" << ' ' << ((end - begin) / scale_tgt) * 1820.0 << ' ' << plevel1 << ' ' << pphase1 << ' ' << pphase2 << endl;
 					
 					// LD only: need to adjust output value for velocity
-					double lvl_adjust = ((((end - begin) / scale_tgt) - 1) * 0.84) + 1;
-					for (int x = 115; (oline > 0) && (x < (115+1685)); x++) {
-						double v = tout3[x];
-
-//						cerr << x << ' ' << tout1[x] << ' ' << tout2[x] << ' ' << tout3[x] << endl;
+					double lvl_adjust = ((((end - begin) / iscale_tgt) - 1) * 0.84) + 1;
+					int ldo = -128;
+					for (int i = 0; (oline > 0) && (i < (213 * out_freq)); i++) {
+						double v = tout3[i + (int)(14 * out_freq)];
 
 						v = ((v / 57344.0) * 1700000) + 7600000;
 						v *= lvl_adjust;
 						v = ((v - 7600000) / 1700000) * 57344.0;
 
+						if ((v < 3000) && (i > 16)) ldo = i;
+
+						if (((i - ldo) < 16) && (i > 4) && (oline >= 4)) {
+							v = frame[oline - 2][i - 2];
+						}
+
 //						cerr << tout3[x] << ' ' << v << endl;
-						frame[oline][x - 115] = clamp(v, 0, 65535);
-//						frame[oline][x - 130] = tout3[x];
+						frame[oline][i] = clamp(v, 0, 65535);
+//						cerr << x << ' ' << tout1[x] << ' ' << tout2[x] << ' ' << tout3[x] << ' ' << v << ' ' << frame[oline][x - (int)(14 * out_freq)] << endl;
 					}
-				
+
+					if (tgt_phase > 0) 
+						frame[oline][0] = 32768; 
+					else
+						frame[oline][0] = 16384; 
+					
 					//crosspoint = begin + (((end - begin) / scale_tgt) * 1820);
 	
 					line++;
@@ -323,12 +363,7 @@ int main(int argc, char *argv[])
 	}
 
 	for (int i = 0; i < 2400; i++) {
-		 burst_hdyne[i] = complex<double>(cos(((i / freq) * 2.0 * M_PIl) + (0.0/180.0)), -(sin(((i / freq) * 2.0 * M_PIl) + (0.0/180.0))));
-	}
-	// build table of standard cos/sin for phase/level calc	
-	for (int e = 0; e < freq; e++) {
-		_cos[e] = cos((2.0 * M_PIl * ((double)e / freq)));
-		_sin[e] = sin((2.0 * M_PIl * ((double)e / freq)));
+		 burst_hdyne[i] = complex<double>(cos(((i / in_freq) * 2.0 * M_PIl) + (0.0/180.0)), -(sin(((i / in_freq) * 2.0 * M_PIl) + (0.0/180.0))));
 	}
 
 	// XX: make parameter
