d9a66ce4c1a4437f282f4a0715c980d19b578d6e
Max Smith <megapixie@yahoo.com>
2015-03-01 12:56:45 +0900

    Make a bit more user friendly for working with rf samples.

--- fft8.py
+++ fft8.py
@@ -1,25 +1,46 @@
-import numpy as np
+#! /usr/bin/python
+import math
 import matplotlib.pyplot as plt
+import numpy as np
 import scipy as sp
-import scipy.signal as sps
 import sys
 
-#binfile = open("ve-greynb50.tbc", "rb").read()
-binfile = open(sys.argv[1], "rb").read()
-#binfile = open("tgen.ld", "rb").read()
-data = np.fromstring(binfile, dtype=np.uint8)
+SAMPLE_RATE = 3.579545 * 8
+MAX_SAMPLES = int(1e6)
 
-mean = np.mean(data)
-c = data - mean
+def load( filename ):
+	if filename.endswith('.ld'):
+		ttype = np.uint16
+		max_samples = MAX_SAMPLES * 2
+		print('Assuming 16 bits per pixel')
+	else:
+		ttype = np.uint8
+		max_samples = MAX_SAMPLES
+		print('Assuming 8 bits per pixel')
+	with open(filename, "rb") as binfile:  
+		data = np.fromstring(binfile.read(max_samples), dtype=ttype)  # Read at most 1e6 samples
+		print('Loaded %d bytes of file %s' % (max_samples, filename))
+	return data	
 
-t = np.arange(len(c))
-sp = np.fft.fft(c)
-freq = np.fft.fftfreq(t.shape[-1])
-d = []
-#for i in range(0, len(data)):
-#        d.append(np.sqrt((sp[i].real * sp[i].real) + (sp[i].imag * sp[i].imag)))
+def load_and_plot( filename ):
+	data     = load( filename )
+	mean     = np.mean(data)
+	rawmax   = max( data )
+	rawmin   = min( data )
+	sigrange = rawmax - rawmin
+	data = data - mean
+	freq = np.fft.fftfreq(np.arange(len(data)).shape[-1]) * SAMPLE_RATE  # Set frequency scale
+	data = np.fft.fft(data)
+	data = np.sqrt((data.real * data.real) + (data.imag * data.imag))
+	data = data / max( data )  # Normalize to y to 1.0
+	peak_to_background = 20 * math.log10( 1.0 / np.mean( data ) )
+	plt.plot(freq[4:], data[4:])  
+	plt.axis( [ 0, SAMPLE_RATE / 2, 0, 1 ] )
+	plt.xlabel( 'Frequency (MHz)' )
+	plt.ylabel( 'Power (linear)' )
+	fig = plt.figure(1)
+	fig.canvas.set_window_title( 'FFT8  %s  Range:%d(%d - %d) Mean:%.2f PkBG:%.2fdb' %\
+					          ( filename, sigrange, rawmax, rawmin, mean, peak_to_background ) )
+	plt.show()
 
-d = np.sqrt((sp.real * sp.real) + (sp.imag * sp.imag))
-
-plt.plot(freq[4:], d[4:])
-plt.show()
+load_and_plot( sys.argv[1] )
