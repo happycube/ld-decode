d64312b3937ecee8471e5da001976d48ac024ece
happycube <Chad.Page@gmail.com>
2015-08-02 13:42:49 -0700

    first bit of ld-decoder.py cleanup.  more coming soon hopefully

--- ld-decoder.py
+++ ld-decoder.py
@@ -9,6 +9,8 @@ import fdls as fdls
 import matplotlib.pyplot as plt
 import fft8 as fft8 
 
+import ld_utils as utils
+
 import getopt
 
 π = np.pi
@@ -18,145 +20,20 @@ import getopt
 
 freq = (315.0 / 88.0) * 8.0
 freq_hz = freq * 1000000.0
+ffreq = freq/2.0
 
 blocklen = (16 * 1024) + 512
 hilbertlen = (16 * 1024)
 
-wide_mode = 0
-
-def dosplot(B, A):
-	w, h = sps.freqz(B, A)
-
-	fig = plt.figure()
-	plt.title('Digital filter frequency response')
-
-	ax1 = fig.add_subplot(111)
-
-	db = 20 * np.log10(abs(h))
-
-	for i in range(1, len(w)):
-		if (db[i] >= -10) and (db[i - 1] < -10):
-			print(">-10db crossing at ", w[i] * (freq/π) / 2.0) 
-		if (db[i] >= -3) and (db[i - 1] < -3):
-			print("-3db crossing at ", w[i] * (freq/π) / 2.0) 
-		if (db[i] < -3) and (db[i - 1] >= -3):
-			print("<-3db crossing at ", w[i] * (freq/π) / 2.0) 
-		if (db[i] < -10) and (db[i - 1] >= -10):
-			print("<-10db crossing at ", w[i] * (freq/π) / 2.0) 
-		if (db[i] < -20) and (db[i - 1] >= -20):
-			print("<-20db crossing at ", w[i] * (freq/π) / 2.0) 
-
-	plt.plot(w * (freq/π) / 2.0, 20 * np.log10(abs(h)), 'b')
-	plt.ylabel('Amplitude [dB]', color='b')
-	plt.xlabel('Frequency [rad/sample]')
-
-	plt.show()
-
-def doplot(B, A):
-	w, h = sps.freqz(B, A)
-
-	fig = plt.figure()
-	plt.title('Digital filter frequency response')
-	
-	db = 20 * np.log10(abs(h))
-	for i in range(1, len(w)):
-		if (db[i] >= -10) and (db[i - 1] < -10):
-			print(">-10db crossing at ", w[i] * (freq/π) / 2.0) 
-		if (db[i] >= -3) and (db[i - 1] < -3):
-			print(">-3db crossing at ", w[i] * (freq/π) / 2.0) 
-		if (db[i] < -3) and (db[i - 1] >= -3):
-			print("<-3db crossing at ", w[i] * (freq/π) / 2.0) 
-
-	ax1 = fig.add_subplot(111)
-	
-	plt.plot(w * (freq/π) / 2.0, 20 * np.log10(abs(h)), 'b')
-	plt.ylabel('Amplitude [dB]', color='b')
-	plt.xlabel('Frequency [rad/sample]')
-
-	ax2 = ax1.twinx()
-	angles = np.unwrap(np.angle(h))
-	plt.plot(w * (freq/π) / 2.0, angles, 'g')
-	plt.ylabel('Angle (radians)', color='g')
-	
-	plt.grid()
-	plt.axis('tight')
-	plt.show()
-
-def doplot2(B, A, B2, A2):
-	w, h = sps.freqz(B, A)
-	w2, h2 = sps.freqz(B2, A2)
-
-#	h.real /= C
-#	h2.real /= C2
-
-	begin = 0
-	end = len(w)
-#	end = int(len(w) * (12 / freq))
-
-#	chop = len(w) / 20
-	chop = 0
-	w = w[begin:end]
-	w2 = w2[begin:end]
-	h = h[begin:end]
-	h2 = h2[begin:end]
-
-	v = np.empty(len(w))
-	
-#	print len(w)
-
-	hm = np.absolute(h)
-	hm2 = np.absolute(h2)
-
-	v0 = hm[0] / hm2[0]
-	for i in range(0, len(w)):
-#		print i, freq / 2 * (w[i] / π), hm[i], hm2[i], hm[i] / hm2[i], (hm[i] / hm2[i]) / v0
-		v[i] = (hm[i] / hm2[i]) / v0
-
-	fig = plt.figure()
-	plt.title('Digital filter frequency response')
-
-	ax1 = fig.add_subplot(111)
-
-	v  = 20 * np.log10(v )
-
-#	plt.plot(w * (freq/π) / 2.0, v)
-#	plt.show()
-#	exit()
-
-	plt.plot(w * (freq/π) / 2.0, 20 * np.log10(abs(h)), 'r')
-	plt.plot(w * (freq/π) / 2.0, 20 * np.log10(abs(h2)), 'b')
-	plt.ylabel('Amplitude [dB]', color='b')
-	plt.xlabel('Frequency [rad/sample]')
-	
-	ax2 = ax1.twinx()
-	angles = np.unwrap(np.angle(h))
-	angles2 = np.unwrap(np.angle(h2))
-	plt.plot(w * (freq/π) / 2.0, angles, 'g')
-	plt.plot(w * (freq/π) / 2.0, angles2, 'y')
-	plt.ylabel('Angle (radians)', color='g')
-
-	plt.grid()
-	plt.axis('tight')
-	plt.show()
-
-ffreq = freq/2.0
-
 lowpass_filter_b, lowpass_filter_a = sps.butter(8, (4.5/(freq/2)), 'low')
 
-#dosplot(lowpass_filter_b, lowpass_filter_a)
-#exit()
+# stubs for later
+f_deemp_b = []
+f_deemp_a = []
 
-#lowpass_filter_b = [1.0]
-#lowpass_filter_a = [1.0]
-
-# demphasis coefficients.  haven't figured out how to compute them outside octave yet.
-
-deemp_t1 = .85
-deemp_t2 = 3.0
-
-# t1 = .85, t2 = 3.0
-f_deemp_b = [3.244425401246140e-01, -2.097191723349937e-01, ]
-f_deemp_a = [1.000000000000000e+00, -8.852766322103798e-01, ]
+# default deemp constants				
+deemp_t1 = .825
+deemp_t2 = 2.35
 
 # audio filters
 Baudiorf = sps.firwin(65, 3.5 / (freq / 2), window='hamming', pass_zero=True)
@@ -173,44 +50,31 @@ N, Wn = sps.buttord([(left_audfreq-.10) / hfreq, (left_audfreq+.10) / hfreq], [(
 leftbp_filter_b, leftbp_filter_a = sps.butter(N, Wn, btype='bandpass')
 
 N, Wn = sps.buttord([(right_audfreq-.10) / hfreq, (right_audfreq+.10) / hfreq], [(right_audfreq-.15) / hfreq, (right_audfreq+.15)/hfreq], 1, 15)
-#print(N,Wn)
-#N, Wn = sps.buttord([(right_audfreq-.10) / hfreq, (right_audfreq+.10) / hfreq], [(right_audfreq-.18) / hfreq, (right_audfreq+.18)/hfreq], 1, 15)
-#print(N,Wn)
 rightbp_filter_b, rightbp_filter_a = sps.butter(N, Wn, btype='bandpass')
-
-#doplot(leftbp_filter_b, leftbp_filter_a)
-#doplot2(leftbp_filter, [1.0], rightbp_filter, [1.0]);
-#doplot2(leftbp_filter_b, leftbp_filter_a, rightbp_filter_b, rightbp_filter_a);
 
 N, Wn = sps.buttord(0.016 / hfreq, 0.024 / hfreq, 1, 8) 
 audiolp_filter_b, audiolp_filter_a = sps.butter(N, Wn)
 
 N, Wn = sps.buttord(3.1 / (freq / 2.0), 3.5 / (freq / 2.0), 1, 16) 
-#N = 12
-#Wn = 3.3 / (freq / 2.0)
-#N = 8
-#Wn = 3.4 / (freq / 2.0)
 audiorf_filter_b, audiorf_filter_a = sps.butter(N, Wn)
 
-# from http://tlfabian.blogspot.com/2013/01/implementing-hilbert-90-degree-shift.html
-hilbert_filter = np.fft.fftshift(
-    np.fft.ifft([0]+[1]*20+[0]*20)
-)
+# unused snippet: from http://tlfabian.blogspot.com/2013/01/implementing-hilbert-90-degree-shift.html
+#hilbert_filter = np.fft.fftshift(
+#    np.fft.ifft([0]+[1]*20+[0]*20)
+#)
 
 def fm_decode(in_filt, freq_hz, hlen = hilbertlen):
 	hilbert = sps.hilbert(in_filt[0:hlen])
 #	hilbert = sps.lfilter(hilbert_filter, 1.0, in_filt)
 
-	# the hilbert transform has errors at the edges.  but it doesn't seem to matter much IRL
+	# the hilbert transform has errors at the edges.  but it doesn't seem to matter much in practice 
 	chop = 256 
 	hilbert = hilbert[chop:len(hilbert)-chop]
 
 	tangles = np.angle(hilbert) 
-
-#	dangles = np.diff(tangles[128:])
 	dangles = np.diff(tangles)
 
-	# make sure unwapping goes the right way
+	# If unwrapping at 0 is negative, flip 'em all around
 	if (dangles[0] < -π):
 		dangles[0] += τ
 	
@@ -245,8 +109,7 @@ minn = 8100000 + (hz_ire_scale * -60)
 
 out_scale = 65534.0 / (maxire - minire)
 	
-Bbpf, Abpf = sps.butter(2, [3.4/(freq/2), 14.0/(freq/2)], btype='bandpass')
-#Bbpf, Abpf = sps.butter(3, 3.2/(freq/2), btype='highpass')
+Bbpf, Abpf = sps.butter(3, [3.2/(freq/2), 14.0/(freq/2)], btype='bandpass')
 Bcutl, Acutl = sps.butter(1, [2.055/(freq/2), 2.505/(freq/2)], btype='bandstop')
 Bcutr, Acutr = sps.butter(1, [2.416/(freq/2), 3.176/(freq/2)], btype='bandstop')
 # AC3 - Bcutr, Acutr = sps.butter(1, [2.68/(freq/2), 3.08/(freq/2)], btype='bandstop')
@@ -273,14 +136,12 @@ def process_video(data):
 	in_filt2 = sps.lfilter(Bcutl, Acutl, in_filt1)
 	in_filt3 = sps.lfilter(Bcutr, Acutr, in_filt2)
 
-	if Inner == 0:
-		in_filt = in_filt3
-	else:
+	if Inner:
 		in_filt = sps.lfilter(f_emp_b, f_emp_a, in_filt3)
-
+	else:
+		in_filt = in_filt3
 
 #	fft8.plotfft(in_filt)
-#	#fft8.plotfft(data)
 #	plt.plot(in_filt)
 #	plt.show()
 
@@ -289,38 +150,15 @@ def process_video(data):
 	# save the original fm decoding and align to filters
 	output_prefilt = output[(len(f_deemp_b) * 24) + len(f_deemp_b) + len(lowpass_filter_b):]
 
-	# clip impossible values (i.e. rot)
-#	output = np.clip(output, 7100000, 10800000) 
-
 	output = sps.lfilter(lowpass_filter_b, lowpass_filter_a, output)
 
 	doutput = (sps.lfilter(f_deemp_b, f_deemp_a, output)[len(f_deemp_b) * 32:len(output)]) 
-
-#	doutput = (sps.lfilter(f_deemp_b, f_deemp_a, doutput)[64:len(doutput)]) / deemp_corr
 	
 	output_16 = np.empty(len(doutput), dtype=np.uint16)
 	reduced = (doutput - minn) / hz_ire_scale
 	output = np.clip(reduced * out_scale, 0, 65535) 
 	
 	np.copyto(output_16, output, 'unsafe')
-
-	err = 1
-	while False: # err > 0:
-		err = 0
-	
-		am = np.argmax(output_prefilt)	
-		if (output_prefilt[np.argmax(output_prefilt)] > 11200000):
-			err = 1
-			output_prefilt[am] = 11200000
-			if (am < len(output_16)):
-				output_16[am] = 0
-		
-		am = np.argmin(output_prefilt)	
-		if (output_prefilt[np.argmax(output_prefilt)] < 7000000):
-			err = 1
-			output_prefilt[am] = 7000000
-			if (am < len(output_16)):
-				output_16[am] = 0
 
 #	plt.plot(range(0, len(data)), data)
 #	plt.plot(range(0, len(output_16)), output_16)
@@ -329,8 +167,6 @@ def process_video(data):
 
 # graph for debug
 #	output = (sps.lfilter(f_deemp_b, f_deemp_a, output)[128:len(output)]) / deemp_corr
-
-#	print output
 
 	plt.plot(range(0, len(output_16)), output_16)
 #	plt.plot(range(0, len(doutput)), doutput)
@@ -369,13 +205,6 @@ def process_audio(indata):
 	in_left = sps.lfilter(leftbp_filter_b, leftbp_filter_a, in_filt4)[len(leftbp_filter_b) * 1:] 
 	in_right = sps.lfilter(rightbp_filter_b, rightbp_filter_a, in_filt4)[len(rightbp_filter_b) * 1:] 
 
-#	if (len(in_left) % 2):
-#		in_left = in_left[0:len(in_left - 1)]
-#	if (len(in_right) % 2):
-#		in_right = in_right[0:len(in_right - 1)]
-
-#	print len(in_left)
-
 	out_left = fm_decode(in_left, freq_hz / 4)
 	out_right = fm_decode(in_right, freq_hz / 4)
 
@@ -393,8 +222,6 @@ def process_audio(indata):
 		outputf[(tot * 2) + 1] = out_right[i]
 		tot = tot + 1
 
-#	exit()
-	
 	return outputf[0:tot * 2], tot * 20 * 4 
 
 	plt.plot(range(0, len(out_left)), out_left)
@@ -408,15 +235,9 @@ def test():
 	test = np.empty(blocklen, dtype=np.uint8)
 #	test = np.empty(blocklen)
 
-	global hilbert_filter
-
 #	for hlen in range(3, 18):
 	for vlevel in range(20, 100, 10):
-#		hilbert_filter = np.fft.fftshift(
-#		    np.fft.ifft([0]+[1]*hlen+[0]*hlen)
-#		)
 
-#		vlevel = 40
 		vphase = 0
 		alphase = 0
 		arphase = 0
@@ -448,7 +269,7 @@ def test():
 
 def main():
 	global lowpass_filter_b, lowpass_filter_a 
-	global wide_mode, hz_ire_scale, minn
+	global hz_ire_scale, minn
 	global f_deemp_b, f_deemp_a
 
 	global deemp_t1, deemp_t2
@@ -494,10 +315,6 @@ def main():
 			Bcutr, Acutr = sps.butter(1, [2.50/(freq/2), 3.26/(freq/2)], btype='bandstop')
 			Bcutr, Acutr = sps.butter(1, [2.68/(freq/2), 3.08/(freq/2)], btype='bandstop')
 		if o == "-w":
-#			lowpass_filter_b, lowpass_filter_a = sps.butter(9, (5.0/(freq/2)), 'low')
-#			lowpass_filter_b, lowpass_filter_a = sps.butter(8, (4.8/(freq/2)), 'low')
-#			lowpass_filter_b, lowpass_filter_a = sps.butter(7, (4.7/(freq/2)), 'low')
-			wide_mode = 1
 			hz_ire_scale = (9360000 - 8100000) / 100
 			minn = 8100000 + (hz_ire_scale * -60)
 		if o == "-S":
@@ -528,9 +345,6 @@ def main():
 				lowpass_filter_b, lowpass_filter_a = sps.butter(6, (4.7/(freq/2)), 'low')
 
 
-#	dosplot(lowpass_filter_b, lowpass_filter_a)
-#	exit()
-
 	argc = len(cut_argv)
 	if argc >= 1:
 		infile = open(cut_argv[0], "rb")
@@ -551,7 +365,6 @@ def main():
 		byte_start *= freq_hz 
 		byte_end *= freq_hz 
 	else:
-		# for backwards compat (for now)
 		byte_end += byte_start
 
 	byte_end -= byte_start
@@ -590,22 +403,14 @@ def main():
 
 		if audio_mode:	
 			output, osamp = process_audio(indata)
-#			for i in range(0, osamp):
-#				print i, output[i * 2], output[(i * 2) + 1]
 			
 			nread = osamp 
-
-#			print len(output), nread
-
 			outfile.write(output)
 		else:
 			output_16 = process_video(indata)
-#			output_16.tofile(outfile)
 			outfile.write(output_16)
 			nread = len(output_16)
 			
-#			print(len(output_16), nread)
-
 			total_pread = total_read 
 			total_read += nread
 
---
+++ ld_utils.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+import numpy as np
+import scipy as sp
+import scipy.signal as sps
+import scipy.fftpack as fftpack 
+import sys
+
+import fdls as fdls
+import matplotlib.pyplot as plt
+import fft8 as fft8
+
+π = np.pi
+τ = np.pi * 2
+
+# Plotting routines
+
+def dosplot(B, A, freq = (315.0/88.0) * 8.0):
+	w, h = sps.freqz(B, A)
+
+	fig = plt.figure()
+	plt.title('Digital filter frequency response')
+
+	ax1 = fig.add_subplot(111)
+
+	db = 20 * np.log10(abs(h))
+
+	for i in range(1, len(w)):
+		if (db[i] >= -10) and (db[i - 1] < -10):
+			print(">-10db crossing at ", w[i] * (freq/π) / 2.0) 
+		if (db[i] >= -3) and (db[i - 1] < -3):
+			print("-3db crossing at ", w[i] * (freq/π) / 2.0) 
+		if (db[i] < -3) and (db[i - 1] >= -3):
+			print("<-3db crossing at ", w[i] * (freq/π) / 2.0) 
+		if (db[i] < -10) and (db[i - 1] >= -10):
+			print("<-10db crossing at ", w[i] * (freq/π) / 2.0) 
+		if (db[i] < -20) and (db[i - 1] >= -20):
+			print("<-20db crossing at ", w[i] * (freq/π) / 2.0) 
+
+	plt.plot(w * (freq/π) / 2.0, 20 * np.log10(abs(h)), 'b')
+	plt.ylabel('Amplitude [dB]', color='b')
+	plt.xlabel('Frequency [rad/sample]')
+
+	plt.show()
+
+def doplot(B, A, freq = (315.0/88.0) * 8.0):
+	w, h = sps.freqz(B, A)
+
+	fig = plt.figure()
+	plt.title('Digital filter frequency response')
+	
+	db = 20 * np.log10(abs(h))
+	for i in range(1, len(w)):
+		if (db[i] >= -10) and (db[i - 1] < -10):
+			print(">-10db crossing at ", w[i] * (freq/π) / 2.0) 
+		if (db[i] >= -3) and (db[i - 1] < -3):
+			print(">-3db crossing at ", w[i] * (freq/π) / 2.0) 
+		if (db[i] < -3) and (db[i - 1] >= -3):
+			print("<-3db crossing at ", w[i] * (freq/π) / 2.0) 
+
+	ax1 = fig.add_subplot(111)
+	
+	plt.plot(w * (freq/π) / 2.0, 20 * np.log10(abs(h)), 'b')
+	plt.ylabel('Amplitude [dB]', color='b')
+	plt.xlabel('Frequency [rad/sample]')
+
+	ax2 = ax1.twinx()
+	angles = np.unwrap(np.angle(h))
+	plt.plot(w * (freq/π) / 2.0, angles, 'g')
+	plt.ylabel('Angle (radians)', color='g')
+	
+	plt.grid()
+	plt.axis('tight')
+	plt.show()
+
+def doplot2(B, A, B2, A2, freq = (315.0/88.0) * 8.0):
+	w, h = sps.freqz(B, A)
+	w2, h2 = sps.freqz(B2, A2)
+
+#	h.real /= C
+#	h2.real /= C2
+
+	begin = 0
+	end = len(w)
+#	end = int(len(w) * (12 / freq))
+
+#	chop = len(w) / 20
+	chop = 0
+	w = w[begin:end]
+	w2 = w2[begin:end]
+	h = h[begin:end]
+	h2 = h2[begin:end]
+
+	v = np.empty(len(w))
+	
+#	print len(w)
+
+	hm = np.absolute(h)
+	hm2 = np.absolute(h2)
+
+	v0 = hm[0] / hm2[0]
+	for i in range(0, len(w)):
+#		print i, freq / 2 * (w[i] / π), hm[i], hm2[i], hm[i] / hm2[i], (hm[i] / hm2[i]) / v0
+		v[i] = (hm[i] / hm2[i]) / v0
+
+	fig = plt.figure()
+	plt.title('Digital filter frequency response')
+
+	ax1 = fig.add_subplot(111)
+
+	v  = 20 * np.log10(v )
+
+#	plt.plot(w * (freq/π) / 2.0, v)
+#	plt.show()
+#	exit()
+
+	plt.plot(w * (freq/π) / 2.0, 20 * np.log10(abs(h)), 'r')
+	plt.plot(w * (freq/π) / 2.0, 20 * np.log10(abs(h2)), 'b')
+	plt.ylabel('Amplitude [dB]', color='b')
+	plt.xlabel('Frequency [rad/sample]')
+	
+	ax2 = ax1.twinx()
+	angles = np.unwrap(np.angle(h))
+	angles2 = np.unwrap(np.angle(h2))
+	plt.plot(w * (freq/π) / 2.0, angles, 'g')
+	plt.plot(w * (freq/π) / 2.0, angles2, 'y')
+	plt.ylabel('Angle (radians)', color='g')
+
+	plt.grid()
+	plt.axis('tight')
+	plt.show()
+
+
+
+ 
