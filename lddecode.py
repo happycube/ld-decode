f64697d42ac0711026ee405f51a3a15d84ed0f38
Chad Page <Chad.Page@gmail.com>
2018-12-17 23:28:59 -0800

    hackishly add --MTF and --MTF_offset back in

--- lddecode.py
+++ lddecode.py
@@ -24,8 +24,8 @@ parser.add_argument('-l', '--length', metavar='length', type=int, help='limit le
 parser.add_argument('-p', '--pal', dest='pal', action='store_true', help='source is in PAL format')
 parser.add_argument('-n', '--ntsc', dest='ntsc', action='store_true', help='source is in NTSC format')
 #parser.add_argument('-c', '--cut', dest='cut', action='store_true', help='cut (to r16) instead of decode')
-#parser.add_argument('-m', '--MTF', metavar='mtf', type=float, default=1.0, help='mtf compensation multiplier')
-#parser.add_argument('--MTF_offset', metavar='mtf_offset', type=float, default=0.0, help='mtf compensation offset')
+parser.add_argument('-m', '--MTF', metavar='mtf', type=float, default=1.0, help='mtf compensation multiplier')
+parser.add_argument('--MTF_offset', metavar='mtf_offset', type=float, default=0.0, help='mtf compensation offset')
 parser.add_argument('-f', '--frame', dest='frame', action='store_true', help='output frames')
 
 args = parser.parse_args()
@@ -61,6 +61,9 @@ foutput = False if not args.frame else True
     
 ldd = LDdecode(filename, outname, loader, frameoutput=foutput, system=system)
 ldd.roughseek(firstframe * 2)
+
+ldd.rf.mtf_mult = args.MTF
+ldd.rf.mtf_offset = args.MTF_offset
 
 for i in range(0, req_frames * 2):
     f = ldd.readfield()
