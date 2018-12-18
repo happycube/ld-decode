35ec706d59e6679b6389dbda5a0ac4ed68f6817b
Chad Page <Chad.Page@gmail.com>
2018-12-17 22:35:39 -0800

    json:  change ntscFieldID case

--- lddecode_core.py
+++ lddecode_core.py
@@ -1261,9 +1261,9 @@ class LDdecode:
 
         if f.rf.system == 'NTSC':
             if f.isFirstField:
-                fi['NTSCFieldID'] = 1 if f.field14 else 3
+                fi['ntscFieldID'] = 1 if f.field14 else 3
             else:
-                fi['NTSCFieldID'] = 4 if f.field14 else 2
+                fi['ntscFieldID'] = 4 if f.field14 else 2
 
         self.fieldinfo.append(fi)
 
