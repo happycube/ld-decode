36a4c4ce5c56c9377db9f7ae23d56a619a07dc4e
Simon Inns <simon.inns@gmail.com>
2019-12-31 16:55:11 +0100

    ld-dropout-correct WIP - Updated headers and removed dangling include

--- tools/ld-dropout-correct/dropoutcorrect.h
+++ tools/ld-dropout-correct/dropoutcorrect.h
@@ -3,7 +3,7 @@
     dropoutcorrect.h
 
     ld-dropout-correct - Dropout correction for ld-decode
-    Copyright (C) 2018-2019 Simon Inns
+    Copyright (C) 2018-2020 Simon Inns
 
     This file is part of ld-decode-tools.
 
