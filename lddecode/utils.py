a9ce5ae9bf896552ed3a920ae5bb0d784d79d034
Chad <Chad.Page@gmail.com>
2020-12-24 09:43:14 -0800

    Some crazy Python that creates a self-contained thread in a function call that returns an object

--- lddecode/utils.py
+++ lddecode/utils.py
@@ -834,7 +834,7 @@ def jsondump_thread(ldd, outname):
     # Lambdas only support one statement, but they can be lists of function calls...
     # ... and you can add them to a created object, so we only need to return the Queue
     q.close = lambda: [q.put(None), q.join()][-1]
-        
+
     # Start the self-contained thread
     t = threading.Thread(target=consume, args=(q, ))
     t.start()
