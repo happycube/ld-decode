544d682fcac86a1f918c381c42cae7ca4bb7ce66
oyvindln <oyvindln@users.noreply.github.com>
2021-08-28 01:38:16 +0200

    Split out doc code to new file as well

---
+++ vhsdecode/doc.py
@@ -0,0 +1,201 @@
+import numpy as np
+from numba import njit
+
+from lddecode.utils import inrange
+import vhsdecode.formats as vhs_formats
+
+
+@njit(cache=True)
+def find_crossings(data, threshold):
+    """Find where the data crosses the set threshold."""
+
+    # We do this by constructing array where positions above
+    # the threshold are marked as true, other sfalse,
+    # and use diff to mark where the value changes.
+    crossings = np.diff(data < threshold)
+    # TODO: See if we can avoid reduntantly looking for both up and
+    # down crossing when we just need one of them.
+    return crossings
+
+
+def find_crossings_dir(data, threshold, look_for_down):
+    """Find where the data crosses the set threshold
+    the look_for_down parameters determines if the crossings returned are down
+    or up crossings.
+    ."""
+    crossings = find_crossings(data, threshold)
+    crossings_pos = np.argwhere(crossings)[:, 0]
+    if len(crossings_pos) <= 0:
+        return []
+    first_cross = crossings_pos[0]
+    if first_cross >= len(data):
+        return []
+    first_crossing_is_down = data[first_cross] > data[first_cross + 1]
+    if first_crossing_is_down == look_for_down:
+        return crossings_pos[::2]
+    else:
+        return crossings_pos[1::2]
+
+
+@njit(cache=True)
+def combine_to_dropouts(crossings_down, crossings_up, merge_threshold):
+    """Combine arrays of up and down crossings, and merge ones with small gaps between them.
+    Intended to be used where up and down crossing levels are different, the two lists will not
+    always alternate or have the same length.
+    Returns a list of start/end tuples.
+    """
+    used = []
+    # TODO: Fix when ending on dropout
+
+    cr_up = iter(crossings_up)
+    last_u = 0
+    # Loop through crossings and combine
+    # TODO: Doing this via a loop is probably not ideal in python,
+    # we may want to look for a way to more directly generate a list of down/up crossings
+    # with hysteresis.
+    for d in crossings_down:
+        if d < last_u:
+            continue
+
+        # If the distance between two dropouts is very small, we merge them.
+        if d - last_u < merge_threshold and len(used) > 0:
+            # Pop the last added dropout and use it's starting point
+            # as the start of the merged one.
+            last = used.pop()
+            d = last[0]
+
+        for u in cr_up:
+            if u > d:
+                used.append((d, u))
+                last_u = u
+                break
+
+    return used
+
+
+def detect_dropouts_rf(field):
+    """Look for dropouts in the input data, based on rf envelope amplitude.
+    Uses either an percentage of the frame average rf level, or an absolute value.
+    TODO: A more advanced algorithm with hysteresis etc.
+    """
+    env = field.data["video"]["envelope"]
+    threshold_p = field.rf.dod_threshold_p
+    threshold_abs = field.rf.dod_threshold_a
+    hysteresis = field.rf.dod_hysteresis
+
+    threshold = 0.0
+    if threshold_abs is not None:
+        threshold = threshold_abs
+    else:
+        # Generate a threshold based on the field envelope average.
+        # This may not be ideal on a field with a lot of droputs,
+        # so we may want to use statistics of the previous averages
+        # to avoid the threshold ending too low.
+        field_average = np.mean(field.data["video"]["envelope"])
+        threshold = field_average * threshold_p
+
+    errlist = []
+
+    crossings_down = find_crossings_dir(env, threshold, True)
+    crossings_up = find_crossings_dir(env, threshold * hysteresis, False)
+
+    if (
+        len(crossings_down) > 0
+        and len(crossings_up) > 0
+        and crossings_down[0] > crossings_up[0]
+        and env[0] < threshold
+    ):
+        # Handle if we start on a dropout by adding a zero at the start since we won't have any
+        # down crossing for it in the data.
+        crossings_down = np.concatenate((np.array([0]), crossings_down), axis=None)
+
+    if len(crossings_down) > 0 and len(crossings_up) > 0:
+        errlist = combine_to_dropouts(
+            crossings_down, crossings_up, vhs_formats.DOD_MERGE_THRESHOLD
+        )
+
+    # Drop very short dropouts that were not merged.
+    # We do this after mergin to avoid removing short consecutive dropouts that
+    # could be merged.
+    errlist = list(filter(lambda s: s[1] - s[0] > vhs_formats.DOD_MIN_LENGTH, errlist))
+
+    rv_lines = []
+    rv_starts = []
+    rv_ends = []
+
+    # Convert to tbc positions.
+    dropouts = dropout_errlist_to_tbc(field, errlist)
+    for r in dropouts:
+        rv_lines.append(r[0] - 1)
+        rv_starts.append(int(r[1]))
+        rv_ends.append(int(r[2]))
+
+    return rv_lines, rv_starts, rv_ends
+
+
+def dropout_errlist_to_tbc(field, errlist):
+    """Convert data from raw data coordinates to tbc coordinates, and splits up
+    multi-line dropouts.
+    """
+    dropouts = []
+
+    if len(errlist) == 0:
+        return dropouts
+
+    # Now convert the above errlist into TBC locations
+    errlistc = errlist.copy()
+
+    lineoffset = -field.lineoffset
+
+    # Remove dropouts occuring before the start of the frame so they don't
+    # cause the rest to be skipped
+    curerr = errlistc.pop(0)
+    while len(errlistc) > 0 and curerr[0] < field.linelocs[field.lineoffset]:
+        curerr = errlistc.pop(0)
+
+    # TODO: This could be reworked to be a bit cleaner and more performant.
+
+    for line in range(field.lineoffset, field.linecount + field.lineoffset):
+        while curerr is not None and inrange(
+            curerr[0], field.linelocs[line], field.linelocs[line + 1]
+        ):
+            start_rf_linepos = curerr[0] - field.linelocs[line]
+            start_linepos = start_rf_linepos / (
+                field.linelocs[line + 1] - field.linelocs[line]
+            )
+            start_linepos = int(start_linepos * field.outlinelen)
+
+            end_rf_linepos = curerr[1] - field.linelocs[line]
+            end_linepos = end_rf_linepos / (
+                field.linelocs[line + 1] - field.linelocs[line]
+            )
+            end_linepos = int(np.round(end_linepos * field.outlinelen))
+
+            first_line = line + 1 + lineoffset
+
+            # If the dropout spans multiple lines, we need to split it up into one for each line.
+            if end_linepos > field.outlinelen:
+                num_lines = end_linepos // field.outlinelen
+
+                # First line.
+                dropouts.append((first_line, start_linepos, field.outlinelen))
+                # Full lines in the middle.
+                for n in range(num_lines - 1):
+                    dropouts.append((first_line + n + 1, 0, field.outlinelen))
+                # leftover on last line.
+                dropouts.append(
+                    (
+                        first_line + (num_lines),
+                        0,
+                        np.remainder(end_linepos, field.outlinelen),
+                    )
+                )
+            else:
+                dropouts.append((first_line, start_linepos, end_linepos))
+
+            if len(errlistc):
+                curerr = errlistc.pop(0)
+            else:
+                curerr = None
+
+    return dropouts
