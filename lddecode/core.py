Fix off-by-one error in PAL RF TBC

lddecode/core.py
@@ -1432,11 +1432,12 @@ def downscale_audio(
 # The Field class contains common features used by NTSC and PAL
 class Field:
     def __init__(
-        self, rf, decode, audio_offset=0, keepraw=True, prevfield=None, initphase=False
+        self, rf, decode, audio_offset=0, keepraw=True, prevfield=None, initphase=False, readloc = 0
     ):
         self.rawdata = decode["input"]
         self.data = decode
         self.initphase = initphase  # used for seeking or first field
+        self.readloc = readloc
 
         self.prevfield = prevfield
 
@@ -2496,13 +2497,17 @@ class Field:
 
         # For output consistency reasons, linecount is set to 313 (i.e. 626 lines)
         # in PAL mode.  This needs to be corrected for RF TBC.
+        startline = self.lineoffset
         lc = self.linecount
-        if self.rf.system == "PAL" and not self.isFirstField:
-            lc = 312
+        endline = startline + lc
+
+        if self.rf.system == 'PAL' and lc == 312:
+            startline += 1
+            endline += 1
 
         output = []
 
-        for l in range(self.lineoffset, self.lineoffset + lc):
+        for l in range(startline, endline):
             scaled = scale(fdata, linelocs[l] - delay, linelocs[l + 1] - delay, linelen)
             output.append(np.round(scaled).astype(np.int16))
 
@@ -3455,6 +3460,7 @@ class LDdecode:
             audio_offset=self.audio_offset,
             prevfield=self.curfield,
             initphase=initphase,
+            readloc = self.rawdecode['startloc']
         )
 
         try: