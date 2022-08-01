3de485a2649308846dd5d32e2d032ba9e0c501c4
lizardgai4 <70610229+lizardgai4@users.noreply.github.com>
2022-08-01 04:23:52 -0700

    updated gen_chroma_vid.sh with all argument options found in ld-chroma-decoder, including chroma-gain and chroma-phase (#49)

--- gen_chroma_vid.sh
+++ gen_chroma_vid.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 input=""
+input_tbc_json=""
 videosystem=""
 audiotrack=""
-startframe=0
-length=0
+chroma_gain=-1 # Not a default value.  It's a sentinel value for the video standards below
+chroma_phase=0
+output_format="yuv422p10le"
+monochrome=0
+chroma_decoder=""
 
 FILTER_COMPLEX="[1:v]format=yuv422p10le[chroma];[0:v][chroma]mergeplanes=0x001112:yuv422p10le[output]"
 
@@ -11,12 +15,45 @@ usage() {
 	echo "Usage: $(basename "$0") [-i input (without .tbc)] [-v videosystem]"
 	echo
 	echo "Options:"
-	echo "-i, --input          Name of the input, without extension. This option is mandatory."
-	echo "-v, --videosystem    Either pal or ntsc. Default is pal"
-	echo "-a, --audio          Optional Audiotrack (*.wav, mp3, aac et. al.) to mux with generated video"
-	echo "-s, --start          Specify the start frame number"
-	echo "-l, --length         Specify the length (number of frames to process)"
-	echo "-f, --full           Sets output to full-frame of video signal e.g 1135x625 PAL & 910x525 NTSC (experimental)"
+	echo "-i, --input                                 Name of the input, without extension. This option is mandatory."
+	echo "-v, --videosystem                           Either pal or ntsc. Default is pal"
+	echo "-a, --audio                                 Optional Audiotrack (*.wav, mp3, aac et. al.) to mux with generated video"
+	echo "-s, --start                                 Specify the start frame number"
+	echo "-l, --length                                Specify the length (number of frames to process)"
+	echo "-f, --full                                  Sets output to full-frame of video signal e.g 1135x625 PAL & 910x525 NTSC (experimental)"
+	echo "--chroma-gain                               Gain factor applied to chroma components (default 1.5 for PAL, 2.0 for NTSC)"
+	echo "--chroma-phase                              Phase rotation applied to chroma components (degrees; default 0.0)"
+	echo "-r, --reverse                               Reverse the field order to second/first (default first/second)"
+	echo "-t, --threads                               Specify the number of concurrent threads (default number of logical CPUs)"
+	echo "-q, --quiet                                 Suppress info and warning messages"
+	echo "--input-json <filename>                     Specify the input JSON file"
+	echo "-p, --output-format <output-format>         Output format (ffmpeg output formats; default yuv422p10le);"
+	echo "                                            RGB48, YUV444P16, GRAY16 pixel formats are supported"
+	echo "-b, --blackandwhite                         Output in black and white"
+	echo "--pad, --output-padding <number>            Pad the output frame to a multiple of this many pixels on"
+	echo "-f, --decoder <decoder>                     Decoder to use (pal2d, transform2d, transform3d, ntsc1d,"
+	echo "                                            ntsc2d, ntsc3d, ntsc3dnoadapt; default automatic)"
+	echo "--ffll, --first_active_field_line <number>  The first visible line of a field."
+	echo "                                                Range 1-259 for NTSC (default: 20),"
+	echo "                                                      2-308 for PAL  (default: 22)"
+	echo "--lfll, --last_active_field_line <number>   The last visible line of a field."
+	echo "                                                Range 1-259 for NTSC (default: 259),"
+	echo "                                                      2-308 for PAL  (default: 308)"
+	echo "--ffrl, --first_active_frame_line <number>  The first visible line of a frame."
+	echo "                                                Range 1-525 for NTSC (default: 40),"
+	echo "                                                      1-620 for PAL  (default: 44)"
+	echo "--lfrl, --last_active_frame_line <number>   The last visible line of a frame."
+	echo "                                                Range 1-525 for NTSC (default: 525),"
+	echo "                                                      1-620 for PAL (default: 620)"
+	echo "-o, --oftest                                NTSC: Overlay the adaptive filter map (only used for testing)"
+	echo "--chroma-nr <number>                        NTSC: Chroma noise reduction level in dB (default 0.0)"
+	echo "--luma-nr <number>                          Luma noise reduction level in dB (default 1.0)"
+	echo "--simple-pal                                Transform: Use 1D UV filter (default 2D)"
+	echo "--transform-mode <mode>                     Transform: Filter mode to use (level, threshold; default threshold)"
+	echo "--transform-threshold <number>              Transform: Uniform similarity threshold in 'threshold' mode (default 0.4)"
+	echo "--transform-thresholds <file>               Transform: File containing per-bin similarity thresholds in 'threshold' mode"
+	echo "--show-ffts                                 Transform: Overlay the input and output FFTs"
+	echo "--ntsc-phase-comp                           Use NTSC QADM decoder taking burst phase into account (BETA)"
 	echo
 	echo "Example: $(basename "$0") -i /media/decoded/tape19 -v pal -a /media/decoded/tape19.wav"
 }
@@ -26,7 +63,7 @@ if [ "$1" = "" ]; then
 	exit 0
 fi
 
-extra_decoder_opts=""
+decoder_opts=""
 
 while [ "$1" != "" ]; do
 	case $1 in
@@ -44,20 +81,111 @@ while [ "$1" != "" ]; do
 		;;
 	-s | --start)
 		shift
-		startframe=$1
+		decoder_opts+="-s $1 "
 		;;
 	-l | --length)
 		shift
-		length=$1
+		decoder_opts+="-l $1 "
 		;;
-
 	-h | --help)
 		usage
 		exit
 		;;
 	-f | --full)
 		shift
-		extra_decoder_opts="${extra_decoder_opts} --full-frame"
+		decoder_opts+="${decoder_opts} --full-frame"
+		;;
+	--chroma-gain)
+		shift
+		chroma_gain=$1
+		;;
+	--chroma-phase)
+		shift
+		chroma_phase=$1
+		;;
+	-r | --reverse)
+		shift
+		decoder_opts+="-r "
+		;;
+	-t | --threads)
+		shift
+		decoder_opts+="-t $1 "
+		;;
+	-q | --quiet)
+		shift
+		decoder_opts+="-q "
+		;;
+	--input-json)
+		shift
+		input_tbc_json=$1
+		;;
+	-p | --output-format)
+		shift
+		output_format=$1
+		;;
+	-b | --blackandwhite)
+		shift
+		monochrome=1
+		;;
+	--pad | --output-padding)
+		shift
+		decoder_opts+="--pad $1 "
+		;;
+	-f | --decoder)
+		shift
+		chroma_decoder=$1
+		;;
+	--ffll | --first_active_field_line)
+		shift
+		decoder_opts+="--ffll $1 "
+		;;
+	--lfll | --last_active_field_line)
+		shift
+		decoder_opts+="--lfll $1 "
+		;;
+	--ffrl | --first_active_frame_line)
+		shift
+		decoder_opts+="--ffrl $1 "
+		;;
+	--lfrl | --last_active_frame_line)
+		shift
+		decoder_opts+="--lfrl $1 "
+		;;
+	-o | --oftest)
+		shift
+		decoder_opts+="-o "
+		;;
+	--chroma-nr)
+		shift
+		decoder_opts+="--chroma-nr $1 "
+		;;
+	--luma-nr)
+		shift
+		decoder_opts+="--luma-nr $1 "
+		;;
+	--simple-pal)
+		shift
+		decoder_opts+="--simple-pal $1 "
+		;;
+	--transform-mode)
+		shift
+		decoder_opts+="--transform-mode $1 "
+		;;
+	--transform-threshold)
+		shift
+		decoder_opts+="--transform-threshold $1 "
+		;;
+	--transform-thresholds)
+		shift
+		decoder_opts+="--transform-thresholds $1 "
+		;;
+	--show-ffts)
+		shift
+		decoder_opts+="--show-ffts "
+		;;
+	--ntsc-phase-comp)
+		shift
+		decoder_opts+="--ntsc-phase-comp "
 		;;
 	*)
 		input=$1
@@ -72,22 +200,21 @@ if [ "$input" = "" ]; then
 	exit 1
 fi
 
-if [ "$startframe" -gt 0 ]; then
-	startframe='--start '$startframe
-else
-	startframe=""
-fi
-
-if [ "$length" -gt 0 ]; then
-	length='--length '$length
-else
-	length=""
-fi
-
 input_stripped=${input%.tbc}
 input_tbc=$input_stripped.tbc
 input_chroma_tbc=${input%.tbc}_chroma.tbc
-input_tbc_json=$input_tbc.json
+
+if [ "$input_tbc_json" = "" ]; then
+	input_tbc_json=$input_tbc.json
+fi
+
+#manual setting of ntsc and pal
+if [ "$chroma_decoder" = "ntsc1d" ] || [ "$chroma_decoder" = "ntsc2d" ] || [ "$chroma_decoder" = "ntsc3d" ] || [ "$chroma_decoder" = "ntsc3dnoadapt" ]; then
+	videosystem="ntsc"
+elif [ "$chroma_decoder" = "pal2d" ] || [ "$chroma_decoder" = "transform2d" ] || [ "$chroma_decoder" = "transform3d" ]; then
+	videosystem="pal"
+fi
+
 if [ "$videosystem" = "" ]; then
 	# Very dumb way of checking if the source is PAL
 	# There is probably a better way of doing this...
@@ -101,8 +228,12 @@ fi
 
 if [ "$videosystem" = "pal" ]; then
 	echo "Processing tbc as PAL"
-	chroma_decoder="transform2d"
-	chroma_gain=1.5
+	if [ "$chroma_gain" = "-1" ]; then
+		chroma_gain=1.5
+	fi
+	if [ "$chroma_decoder" = "" ]; then
+		chroma_decoder="transform2d"
+	fi
 	color_space="bt470bg"
 	color_primaries="bt470bg"
 	color_trc="gamma28"
@@ -110,12 +241,16 @@ fi
 
 if [ "$videosystem" = "ntsc" ]; then
 	echo "Processing tbc as NTSC"
-	chroma_decoder="ntsc2d"
-	chroma_gain=2
+	if [ "$chroma_gain" = "-1" ]; then
+		chroma_gain=2
+	fi
+	if [ "$chroma_decoder" = "" ]; then
+		chroma_decoder="ntsc2d"
+	fi
 	color_space="smpte170m"
 	color_primaries="smpte170m"
 	color_trc="smpte170m"
-	extra_decoder_opts="${extra_decoder_opts} --ntsc-phase-comp"
+	decoder_opts="${decoder_opts} --ntsc-phase-comp"
 fi
 
 if [ -f "$audiotrack" ]; then
@@ -127,21 +262,36 @@ else
 	audio_opts_2=""
 fi
 
-ffmpeg -hide_banner -thread_queue_size 4096 -color_range tv \
+# There might be a better way of supporting monochrome output
+if [ "$monochome" = "1" ]; then
+	ffmpeg -hide_banner -thread_queue_size 4096 -color_range tv \
 	-i <(
 		ld-dropout-correct -i "$input_tbc" --output-json /dev/null - |
-			ld-chroma-decoder --chroma-gain 0 -f mono -p y4m $extra_decoder_opts --input-json "$input_tbc_json" - -
-	) \
-	-i <(
-		ld-dropout-correct -i "$input_chroma_tbc" --input-json "$input_tbc_json" --output-json /dev/null - |
-			ld-chroma-decoder -f $chroma_decoder $extra_decoder_opts --luma-nr 0 --chroma-gain $chroma_gain -p y4m --input-json "$input_tbc_json" - -
+			ld-chroma-decoder --chroma-gain 0 -f mono -p y4m $decoder_opts --input-json "$input_tbc_json" - -
 	) \
 	$audio_opts_1 \
 	-filter_complex $FILTER_COMPLEX \
 	-map "[output]":v -c:v ffv1 -coder 1 -context 1 -g 25 -level 3 -slices 16 -slicecrc 1 -top 1 \
-	-pixel_format yuv422p10le -color_range tv -color_primaries $color_primaries -color_trc $color_trc \
+	-pixel_format $output_format -color_range tv -color_primaries $color_primaries -color_trc $color_trc \
 	-colorspace $color_space $audio_opts_2 \
 	-shortest -y "$input_stripped.mkv"
+else
+	ffmpeg -hide_banner -thread_queue_size 4096 -color_range tv \
+	-i <(
+		ld-dropout-correct -i "$input_tbc" --output-json /dev/null - |
+			ld-chroma-decoder --chroma-gain 0 -f mono -p y4m $decoder_opts --input-json "$input_tbc_json" - -
+	) \
+	-i <(
+		ld-dropout-correct -i "$input_chroma_tbc" --input-json "$input_tbc_json" --output-json /dev/null - |
+			ld-chroma-decoder -f $chroma_decoder $decoder_opts --luma-nr 0 --chroma-gain $chroma_gain --chroma-phase $chroma_phase -p y4m --input-json "$input_tbc_json" - -
+	) \
+	$audio_opts_1 \
+	-filter_complex $FILTER_COMPLEX \
+	-map "[output]":v -c:v ffv1 -coder 1 -context 1 -g 25 -level 3 -slices 16 -slicecrc 1 -top 1 \
+	-pixel_format $output_format -color_range tv -color_primaries $color_primaries -color_trc $color_trc \
+	-colorspace $color_space $audio_opts_2 \
+	-shortest -y "$input_stripped.mkv"
+fi
 
 # Encode internet-friendly clip of previous lossless result:
 #ffmpeg -hide_banner -i $1.mkv -vf scale=in_color_matrix=bt601:out_color_matrix=bt709:768x576,bwdif=1:0:0 -c:v libx264 -preset veryslow -b:v 6M -maxrate 6M -bufsize 6M -pixel_format yuv420p -color_primaries bt709 -color_trc bt709 -colorspace bt709 -aspect 4:3 -c:a libopus -b:a 192k -strict -2 -movflags +faststart -y $1_lossy.mp4
