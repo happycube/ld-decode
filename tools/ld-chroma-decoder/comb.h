4d0e63a2f86867129c59cde1b3a4f34d203661f2
Adam Sampson <ats@offog.org>
2022-08-20 01:55:38 +0100

    Remove unused variable.

--- tools/ld-chroma-decoder/comb.h
+++ tools/ld-chroma-decoder/comb.h
@@ -50,7 +50,6 @@ public:
         double chromaPhase = 0.0;
         bool colorlpf = false;
         bool colorlpf_hq = true;
-        bool whitePoint75 = false;
         qint32 dimensions = 2;
         bool adaptive = true;
         bool showMap = false;
