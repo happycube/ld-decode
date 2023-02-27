27fb54124ff24dc3fabbbb5c42eb9b71691267b9
oyvindln <oyvindln@users.noreply.github.com>
2023-02-27 11:07:52 +0100

    add missing files

---
+++ vhsdecode/format_defs/eiaj.py
@@ -0,0 +1,58 @@
+def get_rfparams_pal_eiaj(RFParams_PAL):
+    RFParams_PAL_EIAJ = {**RFParams_PAL}
+    # EIAJ section
+    # These need tweaking.
+    RFParams_PAL_EIAJ["video_bpf_low"] = 1500000
+    RFParams_PAL_EIAJ["video_bpf_high"] = 6700000
+    RFParams_PAL_EIAJ["video_bpf_order"] = 1
+    RFParams_PAL_EIAJ["video_lpf_extra"] = 6900000
+    RFParams_PAL_EIAJ["video_lpf_extra_order"] = 3
+    RFParams_PAL_EIAJ["video_hpf_extra"] = 1500000
+    RFParams_PAL_EIAJ["video_hpf_extra_order"] = 1
+    RFParams_PAL_EIAJ["video_lpf_freq"] = 4200000
+    RFParams_PAL_EIAJ["video_lpf_order"] = 2
+    # 685546 ± 200
+    RFParams_PAL_EIAJ["color_under_carrier"] = (625 * 25) * (351 / 8)
+    RFParams_PAL_EIAJ["chroma_bpf_upper"] = 1300000
+
+    # Video EQ after FM demod (PAL EIAJ) (based on NTSC one, needs tweak)
+    RFParams_PAL_EIAJ["video_eq"] = {
+        "loband": {"corner": 2.62e6, "transition": 500e3, "order_limit": 20, "gain": 1},
+    }
+
+    # Video Y FM de-emphasis (550 ~ 650ns)
+    RFParams_PAL_EIAJ["deemph_tau"] = 600e-9
+
+    # Temporary video emphasis filter constants
+    # Ideally we would calculate this based on tau and 'x' value, for now
+    # it's eyeballed based on graph and output.
+    RFParams_PAL_EIAJ["deemph_mid"] = 500000
+    RFParams_PAL_EIAJ["deemph_gain"] = 10.8
+
+    # This has not really been stress-tested due to lack of crummy EIAJ samples.
+    RFParams_PAL_EIAJ["boost_bpf_low"] = 5400000
+    RFParams_PAL_EIAJ["boost_bpf_high"] = 6000000
+    RFParams_PAL_EIAJ["boost_bpf_mult"] = 1
+
+    # Needs to be tweaked, just using some random values for now.
+    RFParams_PAL_EIAJ["nonlinear_highpass_freq"] = 1000000
+    RFParams_PAL_EIAJ["nonlinear_highpass_limit_h"] = 5000
+    RFParams_PAL_EIAJ["nonlinear_highpass_limit_l"] = -20000
+
+    return RFParams_PAL_EIAJ
+
+
+def get_sysparams_pal_eiaj(sysparams_pal):
+    SysParams_PAL_EIAJ = {**sysparams_pal}
+
+    # PAL and NTSC "regular-band" use the same frequencies, but
+    # not sure if PAL sync being -43 and ntsc being -40 makes
+    # a difference on these parameters.
+    # SysParams_PAL_EIAJ["ire0"] = 4257143
+    # SysParams_PAL_EIAJ["hz_ire"] = 1600000 / 140.0
+    # SysParams_PAL_EIAJ["burst_abs_ref"] = 5000
+    SysParams_PAL_EIAJ["ire0"] = 4450000  # 4257143
+    SysParams_PAL_EIAJ["hz_ire"] = 1400000 / 140.0
+    SysParams_PAL_EIAJ["burst_abs_ref"] = 1000
+
+    return SysParams_PAL_EIAJ
---
+++ vhsdecode/format_defs/typec.py
@@ -0,0 +1,56 @@
+def get_rfparams_ntsc_typec(RFParams_NTSC):
+    RFParams_NTSC_TYPEC = {**RFParams_NTSC}
+    # TYPEC section
+    # These need tweaking.
+    RFParams_NTSC_TYPEC["video_bpf_low"] = 2500000
+    RFParams_NTSC_TYPEC["video_bpf_high"] = 12000000
+    RFParams_NTSC_TYPEC["video_bpf_order"] = 1
+    RFParams_NTSC_TYPEC["video_lpf_extra"] = 14000000
+    RFParams_NTSC_TYPEC["video_lpf_extra_order"] = 3
+    RFParams_NTSC_TYPEC["video_hpf_extra"] = 1500000
+    RFParams_NTSC_TYPEC["video_hpf_extra_order"] = 1
+    RFParams_NTSC_TYPEC["video_lpf_freq"] = 4200000
+    RFParams_NTSC_TYPEC["video_lpf_order"] = 2
+    # 923828 ± x00
+    RFParams_NTSC_TYPEC["color_under_carrier"] = 3.58e6  # TODO set to fsc properly
+    RFParams_NTSC_TYPEC["chroma_bpf_upper"] = 200000
+
+    # Video EQ after FM demod (NTSC TYPEC) (based on NTSC one, needs tweak)
+    RFParams_NTSC_TYPEC["video_eq"] = {
+        "loband": {"corner": 2.62e6, "transition": 500e3, "order_limit": 20, "gain": 1},
+    }
+
+    # Video Y FM de-emphasis 1
+    RFParams_NTSC_TYPEC["deemph_tau"] = 240e-9
+    # RFParams_NTSC_TYPEC["deemph_tau"] = 600e-9
+
+    # Temporary video emphasis filter constants
+    # Ideally we would calculate this based on tau and 'x' value, for now
+    # it's eyeballed based on graph and output.
+    RFParams_NTSC_TYPEC["deemph_mid"] = 500000
+    RFParams_NTSC_TYPEC["deemph_gain"] = 10.8
+
+    # This has not really been stress-tested due to lack of crummy TYPEC samples.
+    RFParams_NTSC_TYPEC["boost_bpf_low"] = 9500000
+    RFParams_NTSC_TYPEC["boost_bpf_high"] = 10000000
+    RFParams_NTSC_TYPEC["boost_bpf_mult"] = 0
+
+    # Needs to be tweaked, just using some random values for now.
+    RFParams_NTSC_TYPEC["nonlinear_highpass_freq"] = 1000000
+    RFParams_NTSC_TYPEC["nonlinear_highpass_limit_h"] = 5000
+    RFParams_NTSC_TYPEC["nonlinear_highpass_limit_l"] = -20000
+
+    return RFParams_NTSC_TYPEC
+
+
+def get_sysparams_ntsc_typec(sysparams_NTSC):
+    SysParams_NTSC_TYPEC = {**sysparams_NTSC}
+
+    # NTSC and NTSC "regular-band" use the same frequencies, but
+    # not sure if NTSC sync being -43 and ntsc being -40 makes
+    # a difference on these parameters.
+    SysParams_NTSC_TYPEC["hz_ire"] = 2940000 / 140.0
+    SysParams_NTSC_TYPEC["ire0"] = 7.9e6
+    SysParams_NTSC_TYPEC["burst_abs_ref"] = 5000
+
+    return SysParams_NTSC_TYPEC
