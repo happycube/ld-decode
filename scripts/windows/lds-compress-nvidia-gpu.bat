a08dd56d7aa7e910ea72c52dffcec83590c40cf5
Harry Munday <harry@opcomedia.com>
2024-04-02 05:21:45 +0100

    fix script for self contained windows build deployment

--- scripts/windows/lds-compress-nvidia-gpu.bat
+++ scripts/windows/lds-compress-nvidia-gpu.bat
@@ -1,5 +1,5 @@
 @echo off
 title Compressing : %~n1
-"ld-lds-converter.exe" -i "%~1" -u -r | C:\ld-tools-suite-windows\CUETools.FLACCL.cmd.exe -11 -o "%~dp1%~n1.ldf" --lax --ignore-chunk-sizes --task-size 16 --fast-gpu -
+"ld-lds-converter.exe" -i "%~1" -u -r | "CUETools.FLACCL.cmd.exe" -11 -o "%~dp1%~n1.ldf" --lax --ignore-chunk-sizes --task-size 16 --fast-gpu -
 echo Done. 
 PAUSE
