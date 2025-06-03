6acfe3b9eaabcbcb304ac720bf19d6b7cb67fca1
oyvindln <oyvindln@users.noreply.github.com>
2025-06-03 19:47:09 +0200

    fix(ci): make sure build dir exists so versionfile can be placed there

--- scripts/ci/build-windows-decode-bin.py
+++ scripts/ci/build-windows-decode-bin.py
@@ -1,14 +1,20 @@
+import os
 import PyInstaller.__main__
 from pyinstaller_versionfile import create_versionfile
 
 print("Building Windows binary")
+
+# Make sure build directory exists since we place versionfile.txt there.
+build_path = ".\\build"
+if not os.path.isdir(build_path):
+    os.makedirs(build_path)
 
 create_versionfile(
     output_file="build\\versionfile.txt",
     product_name="vhs-decode",
     original_filename="decode.exe",
     file_description="Software defined VHS decoder",
-    #version=version,
+    # version=version,
 )
 
 PyInstaller.__main__.run(
