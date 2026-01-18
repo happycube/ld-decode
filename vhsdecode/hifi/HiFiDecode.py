25fbe57bc2645567ecf4018e8027c582ac3b51e6
Ethan Halsall <ethanhalsall11@augustana.edu>
2026-01-18 17:34:29 -0600

    add citations for filter parameters

--- vhsdecode/hifi/HiFiDecode.py
+++ vhsdecode/hifi/HiFiDecode.py
@@ -43,44 +43,72 @@ from vhsdecode.hifi.utils import DecoderSharedMemory, NumbaAudioArray
 
 import matplotlib.pyplot as plt
 
+# TAU_1         low end of shelf curve
+# TAU_2         high end of shelf curve
+
+# *********************
+# VHS Filter Parameters
+# *********************
 DEFAULT_VHS_EXPANDER_GAIN = 30
+# IEC 60774-2 5.1: Noise Reduction
 DEFAULT_VHS_EXPANDER_RATIO = 2 #           2:1 logarithmic
 DEFAULT_VHS_EXPANDER_ATTACK_TAU = 10e-3 #  3ms to 10ms
-DEFAULT_VHS_EXPANDER_HOLD_TAU = 0
+DEFAULT_VHS_EXPANDER_HOLD_TAU = 0 #        None (only used for 8mm)
 DEFAULT_VHS_EXPANDER_RELEASE_TAU = 70e-3 # 70ms +-20%
 
+# IEC 60774-2 5.2: Pre-emphasis (filter parameters)
+# IEC 60774-2 Figure 2, 4: Pre-emphasis (location in block diagram)
+# for VHS, this deemphasis stage affects both the audio and weighted input to the expander
+DEFAULT_VHS_DEEMPHASIS_TAU_1 = 56e-6 # 56us +- 20%
+DEFAULT_VHS_DEEMPHASIS_TAU_2 = 20e-6 # 20us +- 20%
+
+# IEC 60774-2 Figure 5: Weighting
+DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_1 = 240e-6
+DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_2 = 24e-6
+
+# this low pass filter appears in Panasonic AN3664NFB HiFi signal processing chip, however it is not documented in IEC 60774-2
+# parameters are guessed
+# this may help to remove any high frequency noise interfering with the expander envelope
+DEFAULT_VHS_EXPANDER_WEIGHTING_LOW_PASS = 20000
+DEFAULT_VHS_EXPANDER_WEIGHTING_LOW_PASS_TRANSITION = 100000
+
+# IEC 60774-2 Figure 5: Noise Reduction Emphasis
+DEFAULT_VHS_NR_DEEMPHASIS_TAU_1 = 240e-6
+DEFAULT_VHS_NR_DEEMPHASIS_TAU_2 = 56e-6
+
+
+# *********************
+# 8mm (Video 8, Hi 8) Filter Parameters
+# *********************
 DEFAULT_8MM_EXPANDER_GAIN = 6
+
+# IEC 60843-1 6.5.1.1 Compression Ratio
 DEFAULT_8MM_EXPANDER_RATIO = 2 #           2:1 logarithmic
+
+# IEC 60843-1 6.5.1.2 Transient response
 DEFAULT_8MM_EXPANDER_ATTACK_TAU = 3e-3 #   3ms +- 0.6ms
 DEFAULT_8MM_EXPANDER_HOLD_TAU = 15e-3 #    15ms +- 3ms, gain is held until this time before releasing
 DEFAULT_8MM_EXPANDER_RELEASE_TAU = 40e-3 # 40ms +- 3ms
 
-# TAU_1         low end of shelf curve
-# TAU_2         high end of shelf curve
+# IEC 60843-1 6.5.1.5 Figure 34 Noise Reduction, System Configuration
+# IEC 60843-1 6.5.1.5 Figure 34, Emphasis 1
+DEFAULT_8MM_NR_DEEMPHASIS_TAU_1 = 75e-6
+DEFAULT_8MM_NR_DEEMPHASIS_TAU_2 = 19e-6
 
-# High shelf filter for weighted input to expander
-DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_1 = 240e-6
-DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_2 = 24e-6
-DEFAULT_VHS_EXPANDER_WEIGHTING_LOW_PASS = 20000
-DEFAULT_VHS_EXPANDER_WEIGHTING_LOW_PASS_TRANSITION = 100000
-
-DEFAULT_8MM_EXPANDER_WEIGHTING_TAU_1 = 75e-6
-DEFAULT_8MM_EXPANDER_WEIGHTING_TAU_2 = 27e-6
-DEFAULT_8MM_EXPANDER_WEIGHTING_LOW_PASS = 20000
-DEFAULT_8MM_EXPANDER_WEIGHTING_LOW_PASS_TRANSITION = 100000
-
-# Low shelf filter for deemphasis
-DEFAULT_VHS_DEEMPHASIS_TAU_1 = 56e-6 # 56us +- 20%
-DEFAULT_VHS_DEEMPHASIS_TAU_2 = 20e-6 # 20us +- 20%
-
+# IEC 60843-1 6.5.1.5 Figure 34, Emphasis 2
 DEFAULT_8MM_DEEMPHASIS_TAU_1 = 75e-6
 DEFAULT_8MM_DEEMPHASIS_TAU_2 = 27e-6
 
-DEFAULT_VHS_NR_DEEMPHASIS_TAU_1 = 240e-6
-DEFAULT_VHS_NR_DEEMPHASIS_TAU_2 = 56e-6
+# IEC 60843-1 6.5.1.5 Figure 34, Weighting
+DEFAULT_8MM_EXPANDER_WEIGHTING_TAU_1 = 75e-6
+DEFAULT_8MM_EXPANDER_WEIGHTING_TAU_2 = 27e-6
 
-DEFAULT_8MM_NR_DEEMPHASIS_TAU_1 = 75e-6
-DEFAULT_8MM_NR_DEEMPHASIS_TAU_2 = 19e-6
+# this low pass filter appears in Panasonic AN3664NFB HiFi signal processing chip, however it is not documented in IEC 60843-1
+# parameters are guessed
+# this may help to remove any high frequency noise interfering with the expander envelope
+DEFAULT_8MM_EXPANDER_WEIGHTING_LOW_PASS = 20000
+DEFAULT_8MM_EXPANDER_WEIGHTING_LOW_PASS_TRANSITION = 100000
+
 
 # set the amount of spectral noise reduction to apply to the signal before deemphasis
 DEFAULT_SPECTRAL_NR_AMOUNT = 0.4
