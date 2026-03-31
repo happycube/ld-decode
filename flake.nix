b64d9d1afafed2e46ba1896f6e537dd2714faa04
Simon Inns <simoninns@fastmail.com>
2026-03-31 09:28:44 +0200

    Move from nix unstable to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Software defined LaserDisc decoder";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
